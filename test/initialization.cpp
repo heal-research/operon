7f383302ae7897601516b4eebf41da226a2502ef
Bogdan Burlacu <bogdan.burlacu@fh-hagenberg.at>
2019-09-16 01:40:23 +0200

    Update tree creation unit tests

--- test/initialization.cpp
+++ test/initialization.cpp
@@ -17,15 +17,40 @@ namespace Operon::Test
         auto variables = ds.Variables();
         std::vector<Variable> inputs;
         std::copy_if(variables.begin(), variables.end(), std::back_inserter(inputs), [&](auto& v) { return v.Name != target; });
-        size_t maxDepth = 5, maxLength = 50;
+        size_t maxDepth = 12, maxLength = 50;
 
         auto creator = GrowTreeCreator(maxDepth, maxLength);
         Grammar grammar;
         Operon::Random::JsfRand<64> rd(std::random_device{}());
+        
+        auto trees = std::vector<Tree>(10000);
+        std::generate(trees.begin(), trees.end(), [&]() { return creator(rd, grammar, inputs); });
 
-        auto tree = creator(rd, grammar, inputs);
-        fmt::print("Tree length: {}\n", tree.Length());
-        fmt::print("{}\n", TreeFormatter::Format(tree, ds));
+        auto totalLength = std::transform_reduce(std::execution::par_unseq, trees.begin(), trees.end(), 0.0, std::plus<size_t>{ }, [](const auto& tree) { return tree.Length(); });
+        fmt::print("Full tree creator - length({},{}) = {}\n", maxDepth, maxLength, totalLength / trees.size());
+        auto findIdx = [](uint16_t v) -> gsl::index
+        {
+            gsl::index i = 0;
+            while ((v >>= 1) != 0) { ++i; }
+            return i;
+        };
+
+        std::array<size_t, 14> symbolFrequencies;
+        symbolFrequencies.fill(0u);
+        for(const auto& tree : trees)
+        {
+            for(const auto& node : tree.Nodes())
+            {
+                symbolFrequencies[findIdx(static_cast<uint16_t>(node.Type))]++;
+            }
+        }
+        fmt::print("Symbol frequencies: \n");
+
+        for (size_t i = 0; i < symbolFrequencies.size(); ++i)
+        {
+            auto node = Node(static_cast<NodeType>(1u << i));
+            fmt::print("{}\t{:.3f} %\n", node.Name(), symbolFrequencies[i] / totalLength);
+        }
     }
 
     TEST_CASE("Tree initialization (full)")
@@ -35,15 +60,40 @@ namespace Operon::Test
         auto variables = ds.Variables();
         std::vector<Variable> inputs;
         std::copy_if(variables.begin(), variables.end(), std::back_inserter(inputs), [&](auto& v) { return v.Name != target; });
-        size_t maxDepth = 5, maxLength = 50;
+        size_t maxDepth = 12, maxLength = 50;
 
-        auto creator = FullTreeCreator(maxDepth, maxLength);
+        auto creator = FullTreeCreator(static_cast<int>(std::log2(maxLength)), maxLength);
         Grammar grammar;
         Operon::Random::JsfRand<64> rd(std::random_device{}());
 
-        auto tree = creator(rd, grammar, inputs);
-        fmt::print("Tree length: {}\n", tree.Length());
-        fmt::print("{}\n", TreeFormatter::Format(tree, ds));
+        auto trees = std::vector<Tree>(10000);
+        std::generate(trees.begin(), trees.end(), [&]() { return creator(rd, grammar, inputs); });
+
+        auto totalLength = std::transform_reduce(std::execution::par_unseq, trees.begin(), trees.end(), 0.0, std::plus<size_t>{ }, [](const auto& tree) { return tree.Length(); });
+        fmt::print("Full tree creator - length({},{}) = {}\n", maxDepth, maxLength, totalLength / trees.size());
+        auto findIdx = [](uint16_t v) -> gsl::index
+        {
+            gsl::index i = 0;
+            while ((v >>= 1) != 0) { ++i; }
+            return i;
+        };
+
+        std::array<size_t, 14> symbolFrequencies;
+        symbolFrequencies.fill(0u);
+        for(const auto& tree : trees)
+        {
+            for(const auto& node : tree.Nodes())
+            {
+                symbolFrequencies[findIdx(static_cast<uint16_t>(node.Type))]++;
+            }
+        }
+        fmt::print("Symbol frequencies: \n");
+
+        for (size_t i = 0; i < symbolFrequencies.size(); ++i)
+        {
+            auto node = Node(static_cast<NodeType>(1u << i));
+            fmt::print("{}\t{:.3f} %\n", node.Name(), symbolFrequencies[i] / totalLength);
+        }
     }
 
     TEST_CASE("Tree depth calculation")
