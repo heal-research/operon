fd0bc6f50120ab46e87ff0d1386b741b7817cef2
Bogdan Burlacu <bogdan.burlacu@fh-hagenberg.at>
2019-09-27 23:28:47 +0200

    Commit missing file

---
+++ src/core/metrics.cpp
@@ -0,0 +1,49 @@
+#include "core/metrics.hpp"
+
+namespace Operon {
+double NormalizedMeanSquaredError(gsl::span<const double> x, gsl::span<const double> y)
+{
+    Expects(x.size() == y.size());
+    Expects(x.size() > 0);
+    MeanVarianceCalculator ycalc;
+    MeanVarianceCalculator errcalc;
+    for(int i = 0; i < x.size(); ++i) {
+        if (!std::isnan(y[i])) {
+            ycalc.Add(y[i]);
+        }
+        auto e = x[i] - y[i];
+        errcalc.Add(e * e);
+    }
+    auto yvar = ycalc.NaiveVariance();
+    auto errmean = errcalc.Mean();
+    return yvar > 0 ? errmean / yvar : yvar;
+}
+
+double MeanSquaredError(gsl::span<const double> x, gsl::span<const double> y)
+{
+    Expects(x.size() == y.size());
+    Expects(x.size() > 0);
+    MeanVarianceCalculator mcalc;
+    for(int i = 0; i < x.size(); ++i) {
+        mcalc.Add((x[i] - y[i]) * (x[i] - y[i]));
+    }
+    return mcalc.Mean();
+}
+
+double RootMeanSquaredError(gsl::span<const double> x, gsl::span<const double> y)
+{
+    return std::sqrt(MeanSquaredError(x, y));
+}
+
+double RSquared(gsl::span<const double> x, gsl::span<const double> y)
+{
+    Expects(x.size() == y.size());
+    Expects(x.size() > 0);
+    PearsonsRCalculator pcalc;
+    for(int i = 0; i < x.size(); ++i) {
+        pcalc.Add(x[i], y[i]);
+    }
+    auto r = pcalc.Correlation();
+    return r * r;
+}
+} // namespace Operon
