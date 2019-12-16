16f251542633d27667301e71a47e0e0f26112e43
Bogdan Burlacu <bogdan.burlacu@fh-hagenberg.at>
2019-12-16 16:30:31 +0100

    Add vectorized intersection for hash-based distance (not quite correct yet)

--- include/operon/analyzers/diversity.hpp
+++ include/operon/analyzers/diversity.hpp
@@ -28,6 +28,10 @@
 #include <unordered_set>
 
 namespace Operon {
+    namespace detail {
+        using hash_vector_t = std::vector<operon::hash_t, Eigen::aligned_allocator<operon::hash_t>>;
+    }
+
 template <typename T, typename Scalar = uint32_t>
 class PopulationDiversityAnalyzer final : PopulationAnalyzerBase<T> {
 public:
@@ -75,8 +79,8 @@ public:
         // for large population sizes it is recommended to use the sampling analyzer
         sim = MatrixType::Zero(pop.size(), pop.size());
 
-        std::vector<std::vector<operon::hash_t>> hybrid(pop.size());
-        std::vector<std::vector<operon::hash_t>> strukt(pop.size());
+        std::vector<detail::hash_vector_t> hybrid(pop.size());
+        std::vector<detail::hash_vector_t> strukt(pop.size());
 
         std::vector<gsl::index> indices(pop.size());
         std::iota(indices.begin(), indices.end(), 0);
@@ -91,13 +95,13 @@ public:
         // calculate intersections
         std::for_each(std::execution::par_unseq, indices.begin(), indices.end() - 1, [&](gsl::index i) {
             for (size_t j = i + 1; j < indices.size(); ++j) {
-                sim(i, j) = Intersect(hybrid[i], hybrid[j]);
-                sim(j, i) = Intersect(strukt[i], strukt[j]);
+                sim(i, j) = IntersectVector(hybrid[i], hybrid[j]);
+                sim(j, i) = IntersectVector(strukt[i], strukt[j]);
             }
         });
     }
 
-    static inline Scalar Intersect(const std::vector<operon::hash_t>& lhs, const std::vector<operon::hash_t>& rhs)
+    static Scalar Intersect(const detail::hash_vector_t& lhs, const detail::hash_vector_t& rhs)
     {
         Scalar count = 0;
         size_t i = 0;
@@ -106,23 +110,86 @@ public:
         operon::hash_t lmax = lhs.back();
         operon::hash_t rmax = rhs.back();
 
-        while(i < lhs.size() && j < rhs.size()) {
-            auto a  = lhs[i];
-            auto b  = rhs[j];
-            count  += a == b;
-            i      += a <= b;
-            j      += a >= b;
+        while (i < lhs.size() && j < rhs.size()) {
+            auto a = lhs[i];
+            auto b = rhs[j];
+            count += a == b;
+            i += a <= b;
+            j += a >= b;
 
-            if (a > rmax || b > lmax) { break; }
+            if (a > rmax || b > lmax) {
+                break;
+            }
         }
         return count;
     }
 
-    static inline std::vector<operon::hash_t> HashTree(Tree& tree, bool strict = true)
+    static Scalar IntersectVector(detail::hash_vector_t const& lhs, detail::hash_vector_t const& rhs)
+    {
+        Scalar count = 0;
+        size_t i = 0, j = 0;
+
+        uint64_t const* a = lhs.data();
+        uint64_t const* b = rhs.data();
+
+        // trim lengths to be a multiple of 4
+        size_t aLen = (lhs.size() / 4) * 4;
+        size_t bLen = (rhs.size() / 4) * 4;
+
+        while (i < aLen && j < bLen) {
+            // load segments of four 64-bit elements
+            __m256i v_a                  = _mm256_load_si256((__m256i*)&a[i]);
+            __m256i v_b                  = _mm256_load_si256((__m256i*)&b[j]);
+
+            // move pointers
+            uint64_t a_max               = _mm256_extract_epi64(v_a, 3);
+            uint64_t b_max               = _mm256_extract_epi64(v_b, 3);
+            i                           += (a_max <= b_max) * 4;
+            j                           += (a_max >= b_max) * 4;
+
+            // compute mask of common elements
+            constexpr auto cyclic_shift  = _MM_SHUFFLE(0, 3, 2, 1);
+            __m256i cmp_mask1            = _mm256_cmpeq_epi64(v_a, v_b);
+
+            v_b                          = _mm256_permute4x64_epi64(v_b, cyclic_shift); // shuffling
+            __m256i cmp_mask2            = _mm256_cmpeq_epi64(v_a, v_b);
+
+            v_b                          = _mm256_permute4x64_epi64(v_b, cyclic_shift);
+            __m256i cmp_mask3            = _mm256_cmpeq_epi64(v_a, v_b);
+
+            v_b                          = _mm256_permute4x64_epi64(v_b, cyclic_shift);
+            __m256i cmp_mask4            = _mm256_cmpeq_epi64(v_a, v_b);
+
+            __m256i cmp_mask             = _mm256_or_si256(
+                _mm256_or_si256(cmp_mask1, cmp_mask2),
+                _mm256_or_si256(cmp_mask3, cmp_mask4)); // OR-ing of comparison masks
+
+            // convert the 256-bit mask to 4-bit
+            int mask                     = _mm256_movemask_pd((__m256d)cmp_mask);
+            auto cnt                     = __builtin_popcount(mask); // a number of elements is a weight of the mask
+            count                       += cnt;
+        }
+
+        // intersect the tail using scalar intersection
+        while(i < lhs.size() && j < rhs.size()) {
+            if(a[i] < b[j]) {
+                ++i;
+            } else if(b[j] < a[i]) {
+                ++j;
+            } else {
+                ++count;
+                ++i;
+                ++j;
+            }
+        }
+        return count;
+    }
+
+    static inline std::vector<operon::hash_t, Eigen::aligned_allocator<operon::hash_t>> HashTree(Tree& tree, bool strict = true)
     {
         tree.Sort(strict);
         const auto& nodes = tree.Nodes();
-        std::vector<operon::hash_t> hashes(tree.Length());
+        detail::hash_vector_t hashes(tree.Length());
         std::transform(nodes.begin(), nodes.end(), hashes.begin(), [](const auto& n) { return n.CalculatedHashValue; });
         std::sort(hashes.begin(), hashes.end());
         return hashes;
