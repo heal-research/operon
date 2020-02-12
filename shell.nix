52a73fda3055e831e09d8c1fce6e6b8440a4a004
Bogdan Burlacu <bogdan.burlacu@pm.me>
2020-02-13 00:12:28 +0100

    Update environment script

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
     pkgs = import <nixpkgs> {};
     cxxopts = import ./cxxopts.nix; 
+    qcachegrind = pkgs.libsForQt5.callPackage ./qcachegrind.nix {};
     tbb = pkgs.tbb.overrideAttrs (old: rec {
         installPhase = old.installPhase + ''
         ${pkgs.cmake}/bin/cmake \
@@ -8,6 +9,15 @@ let
             -DSYSTEM_NAME=Linux -DTBB_VERSION_FILE="$out"/include/tbb/tbb_stddef.h \
             -P cmake/tbb_config_installer.cmake
     '';
+    });
+    eigen = pkgs.eigen.overrideAttrs (old: rec {
+        src = pkgs.fetchgit {
+          url             = "https://gitlab.com/libeigen/eigen.git";
+          rev             = "776960024585b907acc4abc3c59aef605941bb75";
+          sha256          = "119h38g9j02jhgf68xm8zzvbswi95amq4nqjqi3nvi6ds6b0k2yk";
+          fetchSubmodules = false;
+        };
+        patches = [ ./include-dir.patch ];
     });
     ceres-solver = pkgs.ceres-solver.overrideAttrs (old: rec {
         cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" "-DCXX11=ON" "-DTBB=ON" "-DBUILD_SHARED_LIBS=ON" ];
@@ -24,7 +34,7 @@ pkgs.gcc9Stdenv.mkDerivation {
 
     buildInputs = with pkgs; [
         # python environment for bindings and scripting
-        (pkgs.python37.withPackages (ps: with ps; [ pip numpy pandas cython scikitlearn pybind11 colorama coloredlogs ]))
+        (pkgs.python37.withPackages (ps: with ps; [ pip numpy pandas cython scikitlearn pybind11 colorama coloredlogs seaborn ]))
         # Project dependencies
         bear # generate compilation database
         gdb
@@ -33,10 +43,16 @@ pkgs.gcc9Stdenv.mkDerivation {
         cmake
         cxxopts
         eigen
+        openlibm
+        gperftools
+        jemalloc
         fmt
         glog
         ceres-solver
         tbb
         catch2
+        # visualize profile results
+        qcachegrind
+        massif-visualizer
     ];
 }
