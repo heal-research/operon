61608ee430d53319463e7b3d8ec47211f5e1cb80
Bogdan Burlacu <bogdan.burlacu@pm.me>
2020-08-12 08:43:20 +0200

    eval detail: make methods static

--- include/operon/core/eval_detail.hpp
+++ include/operon/core/eval_detail.hpp
@@ -32,61 +32,60 @@ namespace detail {
     struct op {
         using Arg = Eigen::Ref<typename Eigen::DenseBase<Eigen::Array<T, BATCHSIZE, Eigen::Dynamic, Eigen::ColMajor>>::ColXpr, Eigen::Unaligned, Eigen::Stride<BATCHSIZE, 1>>;
 
-        inline void apply(Arg ret, Arg arg1) { ret = arg1; }
+        static inline void apply(Arg ret, Arg arg1) { ret = arg1; }
 
         template <typename... Args>
-        inline void apply(Arg ret, Arg arg1, Args... args) { ret = arg1 + (args + ...); }
+        static inline void apply(Arg ret, Arg arg1, Args... args) { ret = arg1 + (args + ...); }
 
-        inline void accumulate(Arg ret, Arg arg1) { ret += arg1; }
+        static inline void accumulate(Arg ret, Arg arg1) { ret += arg1; }
 
         template <typename... Args>
-        inline void accumulate(Arg ret, Arg arg1, Args... args) { ret += arg1 + (args + ...); }
+        static inline void accumulate(Arg ret, Arg arg1, Args... args) { ret += arg1 + (args + ...); }
     };
 
     template <typename T>
     struct op<T, Operon::NodeType::Sub> {
         using Arg = Eigen::Ref<typename Eigen::DenseBase<Eigen::Array<T, BATCHSIZE, Eigen::Dynamic, Eigen::ColMajor>>::ColXpr, Eigen::Unaligned, Eigen::Stride<BATCHSIZE, 1>>;
 
-        inline void apply(Arg ret, Arg arg1) { ret = -arg1; }
+        static inline void apply(Arg ret, Arg arg1) { ret = -arg1; }
 
         template <typename... Args>
-        inline void apply(Arg ret, Arg arg1, Args... args) { ret = arg1 - (args + ...); }
+        static inline void apply(Arg ret, Arg arg1, Args... args) { ret = arg1 - (args + ...); }
 
-        inline void accumulate(Arg ret, Arg arg1) { ret -= arg1; }
+        static inline void accumulate(Arg ret, Arg arg1) { ret -= arg1; }
 
         template <typename... Args>
-        inline void accumulate(Arg ret, Arg arg1, Args... args) { ret -= arg1 + (args + ...); }
+        static inline void accumulate(Arg ret, Arg arg1, Args... args) { ret -= arg1 + (args + ...); }
     };
 
     template <typename T>
     struct op<T, Operon::NodeType::Mul> {
         using Arg = Eigen::Ref<typename Eigen::DenseBase<Eigen::Array<T, BATCHSIZE, Eigen::Dynamic, Eigen::ColMajor>>::ColXpr, Eigen::Unaligned, Eigen::Stride<BATCHSIZE, 1>>;
 
-        inline void apply(Arg ret, Arg arg1) { ret = arg1; }
+        static inline void apply(Arg ret, Arg arg1) { ret = arg1; }
 
         template <typename... Args>
-        inline void apply(Arg ret, Arg arg1, Args... args) { ret = arg1 * (args * ...); }
+        static inline void apply(Arg ret, Arg arg1, Args... args) { ret = arg1 * (args * ...); }
 
-        inline void accumulate(Arg ret, Arg arg1) { ret *= arg1; }
+        static inline void accumulate(Arg ret, Arg arg1) { ret *= arg1; }
 
         template <typename... Args>
-        inline void accumulate(Arg ret, Arg arg1, Args... args) { ret *= arg1 * (args * ...); }
+        static inline void accumulate(Arg ret, Arg arg1, Args... args) { ret *= arg1 * (args * ...); }
     };
 
     template <typename T>
     struct op<T, Operon::NodeType::Div> {
         using Arg = Eigen::Ref<typename Eigen::DenseBase<Eigen::Array<T, BATCHSIZE, Eigen::Dynamic, Eigen::ColMajor>>::ColXpr, Eigen::Unaligned, Eigen::Stride<BATCHSIZE, 1>>;
 
-        template <bool acc = false>
-        inline void apply(Arg ret, Arg arg1) { ret = arg1.inverse(); }
+        static inline void apply(Arg ret, Arg arg1) { ret = arg1.inverse(); }
 
         template <typename... Args>
-        inline void apply(Arg ret, Arg arg1, Args... args) { ret = arg1 / (args * ...); }
+        static inline void apply(Arg ret, Arg arg1, Args... args) { ret = arg1 / (args * ...); }
 
-        inline void accumulate(Arg ret, Arg arg1) { ret /= arg1; }
+        static inline void accumulate(Arg ret, Arg arg1) { ret /= arg1; }
 
         template <typename... Args>
-        inline void accumulate(Arg ret, Arg arg1, Args... args) { ret /= arg1 * (args * ...); }
+        static inline void accumulate(Arg ret, Arg arg1, Args... args) { ret /= arg1 * (args * ...); }
     };
 
     // dispatching mechanism
@@ -97,7 +96,7 @@ namespace detail {
     template <typename T, Operon::NodeType N>
     inline void dispatch_op(Eigen::DenseBase<Eigen::Array<T, BATCHSIZE, Eigen::Dynamic, Eigen::ColMajor>>& m, Operon::Vector<Node> const& nodes, size_t parentIndex)
     {
-        op<T, N> op;
+        using f = op<T, N>;
 
         int arity = nodes[parentIndex].Arity;
         auto nextArg = [&](size_t i) { return i - (nodes[i].Length + 1); };
@@ -110,31 +109,31 @@ namespace detail {
             if (continued) {
                 switch (arity) {
                 case 1: {
-                    op.accumulate(result, m.col(arg1));
+                    f::accumulate(result, m.col(arg1));
                     arity = 0;
                     break;
                 }
                 case 2: {
                     auto arg2 = nextArg(arg1);
-                    op.accumulate(result, m.col(arg1), m.col(arg2));
+                    f::accumulate(result, m.col(arg1), m.col(arg2));
                     arity = 0;
                     break;
                 }
                 case 3: {
                     auto arg2 = nextArg(arg1), arg3 = nextArg(arg2);
-                    op.accumulate(result, m.col(arg1), m.col(arg2), m.col(arg3));
+                    f::accumulate(result, m.col(arg1), m.col(arg2), m.col(arg3));
                     arity = 0;
                     break;
                 }
                 case 4: {
                     auto arg2 = nextArg(arg1), arg3 = nextArg(arg2), arg4 = nextArg(arg3);
-                    op.accumulate(result, m.col(arg1), m.col(arg2), m.col(arg3), m.col(arg4));
+                    f::accumulate(result, m.col(arg1), m.col(arg2), m.col(arg3), m.col(arg4));
                     arity = 0;
                     break;
                 }
                 default: {
                     auto arg2 = nextArg(arg1), arg3 = nextArg(arg2), arg4 = nextArg(arg3), arg5 = nextArg(arg4);
-                    op.accumulate(result, m.col(arg1), m.col(arg2), m.col(arg3), m.col(arg4), m.col(arg5));
+                    f::accumulate(result, m.col(arg1), m.col(arg2), m.col(arg3), m.col(arg4), m.col(arg5));
                     arity -= 5;
                     arg1 = nextArg(arg5);
                     break;
@@ -143,31 +142,31 @@ namespace detail {
             } else {
                 switch (arity) {
                 case 1: {
-                    op.apply(result, m.col(arg1));
+                    f::apply(result, m.col(arg1));
                     arity = 0;
                     break;
                 }
                 case 2: {
                     auto arg2 = nextArg(arg1);
-                    op.apply(result, m.col(arg1), m.col(arg2));
+                    f::apply(result, m.col(arg1), m.col(arg2));
                     arity = 0;
                     break;
                 }
                 case 3: {
                     auto arg2 = nextArg(arg1), arg3 = nextArg(arg2);
-                    op.apply(result, m.col(arg1), m.col(arg2), m.col(arg3));
+                    f::apply(result, m.col(arg1), m.col(arg2), m.col(arg3));
                     arity = 0;
                     break;
                 }
                 case 4: {
                     auto arg2 = nextArg(arg1), arg3 = nextArg(arg2), arg4 = nextArg(arg3);
-                    op.apply(result, m.col(arg1), m.col(arg2), m.col(arg3), m.col(arg4));
+                    f::apply(result, m.col(arg1), m.col(arg2), m.col(arg3), m.col(arg4));
                     arity = 0;
                     break;
                 }
                 default: {
                     auto arg2 = nextArg(arg1), arg3 = nextArg(arg2), arg4 = nextArg(arg3), arg5 = nextArg(arg4);
-                    op.apply(result, m.col(arg1), m.col(arg2), m.col(arg3), m.col(arg4), m.col(arg5));
+                    f::apply(result, m.col(arg1), m.col(arg2), m.col(arg3), m.col(arg4), m.col(arg5));
                     arity -= 5;
                     arg1 = nextArg(arg5);
                     break;
@@ -181,36 +180,34 @@ namespace detail {
     template <typename T, Operon::NodeType N>
     inline void dispatch_op_simple_binary(Eigen::DenseBase<Eigen::Array<T, BATCHSIZE, Eigen::Dynamic, Eigen::ColMajor>>& m, Operon::Vector<Node> const& nodes, size_t parentIndex)
     {
-        op<T, N> op;
         auto r = m.col(parentIndex);
         size_t i = parentIndex - 1;
         size_t arity = nodes[parentIndex].Arity;
 
         if (arity == 1) {
-            op.apply(r, m.col(i));
+            op<T, N>::apply(r, m.col(i));
         } else {
             auto j = i - (nodes[i].Length + 1);
-            op.apply(r, m.col(i), m.col(j));
+            op<T, N>::apply(r, m.col(i), m.col(j));
         }
     }
 
     template <typename T, Operon::NodeType N>
     inline void dispatch_op_simple_nary(Eigen::DenseBase<Eigen::Array<T, BATCHSIZE, Eigen::Dynamic, Eigen::ColMajor>>& m, Operon::Vector<Node> const& nodes, size_t parentIndex)
     {
-        op<T, N> op;
         auto r = m.col(parentIndex);
         size_t arity = nodes[parentIndex].Arity;
 
         auto i = parentIndex - 1;
 
         if (arity == 1) {
-            op.apply(r, m.col(i));
+            op<T, N>::apply(r, m.col(i));
         } else {
             r = m.col(i);
 
             for (size_t k = 1; k < arity; ++k) {
                 i -= nodes[i].Length + 1;
-                op.accumulate(r, m.col(i));
+                op<T, N>::accumulate(r, m.col(i));
             }
         }
     }
