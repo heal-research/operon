a4defe657c29c361cd88c7957467eb39ac91a779
Bogdan Burlacu <bogdan.burlacu@pm.me>
2020-10-14 18:12:13 +0200

    more intuitive random methods

--- include/operon/random/random.hpp
+++ include/operon/random/random.hpp
@@ -13,30 +13,21 @@
 #include <fmt/core.h>
 
 namespace Operon::Random {
-template <typename R, typename T>
-T UniformInt(R& random, T lo, T hi)
+template<typename R, typename T>
+T Uniform(R& random, T a, T b) 
 {
-    static_assert(std::is_invocable_r<uint64_t, R>::value);
-    static_assert(std::is_integral_v<T>);
-    EXPECT(lo < hi);
-    // in our version, the upper limit is exclusive
-    return std::uniform_int_distribution<T>(lo, hi - 1)(random);
-}
-
-template <typename R, typename T>
-T UniformReal(R& random, T lo, T hi)
-{
-    static_assert(std::is_invocable_r<uint64_t, R>::value);
-    static_assert(std::is_floating_point_v<T>);
-    EXPECT(lo < hi);
-    return std::uniform_real_distribution<T>(lo, hi)(random);
+    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
+    if constexpr(std::is_integral_v<T>) {
+        return std::uniform_int_distribution<T>(a,b)(random);
+    } else if constexpr(std::is_floating_point_v<T>) {
+        return std::uniform_real_distribution<T>(a,b)(random);
+    }
 }
 
 template <typename R, typename InputIterator>
 InputIterator Sample(R& random, InputIterator start, InputIterator end)
 {
-    EXPECT(start < end);
-    std::advance(start, UniformInt(random, 0l, std::distance(start, end)));
+    std::advance(start, Uniform(random, 0l, std::distance(start, end)-1));
     return start;
 }
 
@@ -44,13 +35,12 @@ template <typename R, typename InputIterator>
 InputIterator Sample(R& random, InputIterator start, InputIterator end,
     std::add_pointer_t<bool(typename InputIterator::value_type const&)> condition)
 {
-    EXPECT(start < end);
     auto n = std::count_if(start, end, condition);
 
     if (n == 0)
         return end; // no element satisfies the condition
 
-    auto m = UniformInt(random, 0l, n);
+    auto m = Uniform(random, 0l, n-1);
     InputIterator it;
     for (it = start; it < end; ++it) {
         if (condition(*it) && 0 == m--)
