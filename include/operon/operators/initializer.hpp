761ddbd8225808e3aa927c33181e864841e00eef
Bogdan Burlacu <bogdan.burlacu@pm.me>
2020-10-19 16:32:40 +0200

    minor fixes for both gcc/msvc

--- include/operon/operators/initializer.hpp
+++ include/operon/operators/initializer.hpp
@@ -37,7 +37,7 @@ public:
 
     Tree operator()(Operon::RandomGenerator& random) const override
     {
-        auto targetLen = std::max(1ull, static_cast<size_t>(std::round(dist_(random))));
+        auto targetLen = std::max(size_t{1}, static_cast<size_t>(std::round(dist_(random))));
         return creator_(random, targetLen, minDepth_, maxDepth_);
     }
 
