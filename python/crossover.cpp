e4dbde5e3e052fdfb0ae7c1195a5af4f97270364
Bogdan Burlacu <bogdan.burlacu@pm.me>
2020-10-29 18:29:35 +0100

    extend python wrapper

--- python/crossover.cpp
+++ python/crossover.cpp
@@ -6,6 +6,9 @@ void init_crossover(py::module_ &m)
     py::class_<Operon::CrossoverBase>(m, "CrossoverBase");
 
     py::class_<Operon::SubtreeCrossover, Operon::CrossoverBase>(m, "SubtreeCrossover")
-        .def(py::init<double, size_t, size_t>())
+        .def(py::init<double, size_t, size_t>(),
+                py::arg("internal_probability"),
+                py::arg("depth_limit"),
+                py::arg("length_limit"))
         .def("__call__", &Operon::SubtreeCrossover::operator());
 }
