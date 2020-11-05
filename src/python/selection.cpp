18f0aabee02bb3d541c3d78a154a8061192e67f4
Bogdan Burlacu <bogdan.burlacu@pm.me>
2020-11-05 23:27:26 +0100

    pyoperon: add missing init methods to selectors

--- src/python/selection.cpp
+++ src/python/selection.cpp
@@ -14,12 +14,18 @@ void init_selection(py::module_ &m)
         .def_property("TournamentSize", &Operon::TournamentSelector::GetTournamentSize, &Operon::TournamentSelector::SetTournamentSize);
 
     py::class_<Operon::RankTournamentSelector, Operon::SelectorBase>(m, "RankTournamentSelector")
+        .def(py::init([](size_t i){
+                    return Operon::RankTournamentSelector([i](const auto& a, const auto& b) { return a[i] < b[i]; });
+                    }), py::arg("objective_index"))
         .def(py::init<Operon::ComparisonCallback const&>())
         .def("__call__", &Operon::RankTournamentSelector::operator())
         .def("Prepare", &Operon::RankTournamentSelector::Prepare)
         .def_property("TournamentSize", &Operon::RankTournamentSelector::GetTournamentSize, &Operon::RankTournamentSelector::SetTournamentSize);
 
     py::class_<Operon::ProportionalSelector, Operon::SelectorBase>(m, "ProportionalSelector")
+        .def(py::init([](size_t i){
+                    return Operon::ProportionalSelector([i](const auto& a, const auto& b) { return a[i] < b[i]; });
+                    }), py::arg("objective_index"))
         .def(py::init<Operon::ComparisonCallback const&>())
         .def("__call__", &Operon::ProportionalSelector::operator())
         .def("Prepare", py::overload_cast<const gsl::span<const Operon::Individual>>(&Operon::ProportionalSelector::Prepare, py::const_))
