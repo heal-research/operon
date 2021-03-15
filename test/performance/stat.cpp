c1b8bd13df83797372c40d92f89c9a93d079262f
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-03-15 23:35:08 +0100

    optimize batch-mode linear scaling, rename alpha, beta to offset, scale

--- test/performance/stat.cpp
+++ test/performance/stat.cpp
@@ -2,11 +2,9 @@
 #include <core/types.hpp>
 #include <doctest/doctest.h>
 
-#include <stat/meanvariance.hpp>
-#include <stat/meanvariance2.hpp>
-#include <stat/meanvariance3.hpp>
-
 #include <random/random.hpp>
+#include <stat/linearscaler.hpp>
+#include <stat/meanvariance.hpp>
 #include <stat/pearson.hpp>
 
 namespace nb = ankerl::nanobench;
@@ -28,187 +26,48 @@ namespace Test {
         return { mean, var };
     }
 
-    TEST_CASE("random series")
+    TEST_CASE("Stat")
     {
-        constexpr int N = 1000000;
+        const int N = int(1e5);
 
-        std::vector<double> vec(N);
-        Operon::RandomGenerator rng(1234);
-        std::uniform_real_distribution<double> dist(-5, 5);
-        std::generate(vec.begin(), vec.end(), [&]() { return dist(rng); });
+        Eigen::Array<Operon::Scalar, Eigen::Dynamic, 1> x = Eigen::Array<Operon::Scalar, Eigen::Dynamic, 1>::Random(N, 1);
 
-        SUBCASE("performance comparison")
-        {
-            nb::Bench b;
-            b.relative(true);
-            b.performanceCounters(true);
+        decltype(x) y = 5 * x + 3;
 
-            Operon::MeanVarianceCalculator calc1;
-            Operon::MeanVariance2 calc2;
-            Operon::MeanVariance3 calc3;
+        nb::Bench bench;
+        bench.title("Stat").relative(true).performanceCounters(true).minEpochIterations(100);
 
-            auto [m2, v2] = TestCalculator(b, calc2, vec, "welford");
+        gsl::span<Operon::Scalar const> xx(x.data(), x.size());
+        gsl::span<Operon::Scalar const> yy(y.data(), y.size());
 
-            b.run("welford-batch", [&]() {
-                calc2.Reset();
-                calc2.Add(vec);
-            });
+        auto [a1, b1] = Operon::LinearScalingCalculator::Calculate(xx.begin(), xx.end(), yy.begin());
+        auto [a2, b2] = Operon::LinearScalingCalculator::Calculate(xx, yy);
 
-            auto meanWelfordBatch = calc2.Mean();
-            auto varWelfordBatch = calc2.NaiveVariance();
-
-            b.run("welford-eigen", [&]() {
-                calc2.Reset();
-                calc2.AddSIMD(vec);
-            });
-
-            auto meanWelfordEigen = calc2.Mean();
-            auto varWelfordEigen = calc2.NaiveVariance();
-
-            auto [m1, v1] = TestCalculator(b, calc1, vec, "operon-youngs-cramer");
-
-            b.run("operon-youngs-cramer-two-pass", [&]() {
-                calc1.Reset();
-                calc1.AddTwoPass(vec);
-            });
-
-            auto meanOperonTwoPass = calc1.Mean();
-            auto varOperonTwoPass = calc1.NaiveVariance();
-
-            b.run("operon-youngs-cramer-eigen", [&]() {
-                calc1.Reset();
-                calc1.Add(vec);
-            });
-
-            auto meanOperonEigen = calc1.Mean();
-            auto varOperonEigen = calc1.NaiveVariance();
-
-            auto [m3, v3] = TestCalculator(b, calc3, vec, "youngs-cramer");
-
-            b.run("youngs-cramer-batch", [&]() {
-                calc3.Reset();
-                calc3.Add(vec);
-            });
-
-            auto meanYCBatch = calc3.Mean();
-            auto varYCBatch = calc3.NaiveVariance();
-
-            b.run("youngs-cramer-eigen", [&]() {
-                calc3.Reset();
-                calc3.AddSIMD(vec);
-            });
-
-            auto meanYCEigen = calc3.Mean();
-            auto varYCEigen = calc3.NaiveVariance();
-
-            fmt::print("operon:              [{}, {}]\n", m1, v1);
-            fmt::print("operon-twopass:      [{}, {}]\n", meanOperonTwoPass, varOperonTwoPass);
-            fmt::print("operon-eigen         [{}, {}]\n", meanOperonEigen, varOperonEigen);
-            fmt::print("welford:             [{}, {}]\n", m2, v2);
-            fmt::print("welford-batch        [{}, {}]\n", meanWelfordBatch, varWelfordBatch);
-            fmt::print("welford-eigen        [{}, {}]\n", meanWelfordEigen, varWelfordEigen);
-            fmt::print("youngs-cramer:       [{}, {}]\n", m3, v3);
-            fmt::print("youngs-cramer-batch: [{}, {}]\n", meanYCBatch, varYCBatch);
-            fmt::print("youngs-cramer-eigen: [{}, {}]\n", meanYCEigen, varYCEigen);
-        }
-
-        SUBCASE("schubert")
-        {
-            Operon::MeanVarianceCalculator calc;
-            nb::Bench b;
-            b.performanceCounters(true);
-            auto [m, v] = TestCalculator(b, calc, vec, "schubert");
-            fmt::print("[{}, {}]\n", m, v);
-        }
-
-        SUBCASE("welford")
-        {
-            Operon::MeanVariance2 calc;
-            nb::Bench b;
-            b.performanceCounters(true);
-            auto [m, v] = TestCalculator(b, calc, vec, "welford");
-            fmt::print("[{}, {}]\n", m, v);
-            fmt::print("ss = {}\n", calc.SS());
-
-            b.run("welford-batch", [&]() {
-                calc.Reset();
-                calc.Add(vec);
-            });
-            fmt::print("[{}, {}]\n", calc.Mean(), calc.NaiveVariance());
-            fmt::print("ss = {}\n", calc.SS());
-
-            b.run("welford-eigen", [&]() {
-                calc.Reset();
-                calc.AddSIMD(vec);
-            });
-            fmt::print("[{}, {}]\n", calc.Mean(), calc.NaiveVariance());
-            fmt::print("ss = {}\n", calc.SS());
-        }
-
-        SUBCASE("youngs-cramer")
-        {
-            Operon::MeanVariance3 calc;
-            nb::Bench b;
-            b.performanceCounters(true);
-            auto [m, v] = TestCalculator(b, calc, vec, "youngs-cramer");
-            fmt::print("[{}, {}]\n", m, v);
-        }
-    }
-
-    TEST_CASE("correlation")
-    {
+        double a3, b3;
         Operon::PearsonsRCalculator calc;
+        calc.Add(xx, yy);
+        a3 = calc.SampleCovariance() / calc.SampleVarianceX();
+        b3 = calc.MeanY() - a3 * calc.MeanX();
 
-        constexpr int N = 1000000;
-        Operon::Vector<double> x(N), y(N);
-        Operon::RandomGenerator rng(1234);
-        std::uniform_real_distribution<double> dist(-5, 5);
-        std::generate(x.begin(), x.end(), [&]() { return dist(rng); });
-        std::generate(y.begin(), y.end(), [&]() { return dist(rng); });
-        //std::vector<double> x { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        //std::vector<double> y { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        CHECK(a1 == a2);
+        CHECK(b1 == b2);
 
-        SUBCASE("performance comparison")
-        {
-            nb::Bench b;
-            b.performanceCounters(true).relative(true).minEpochIterations(100);
+        CHECK(a1 == a3);
+        CHECK(b1 == b3);
 
-            calc.Reset();
-            for (size_t i = 0; i < N; ++i) {
-                calc.Add(x[i], y[i]);
-            }
-            fmt::print(" SEQ  sumWe: {:.20}, sumX: {:.20}, sumY: {:.20}, sumXX: {:.20}, sumYY: {:.20}, sumXY: {:.20}\n", calc.SumWe(), calc.SumX(), calc.SumY(), calc.SumXX(), calc.SumYY(), calc.SumXY());
+        double f = 0;
 
-            calc.Reset();
-            calc.Add(x, y);
-            fmt::print("SIMD  sumWe: {:.20}, sumX: {:.20}, sumY: {:.20}, sumXX: {:.20}, sumYY: {:.20}, sumXY: {:.20}\n", calc.SumWe(), calc.SumX(), calc.SumY(), calc.SumXX(), calc.SumYY(), calc.SumXY());
+        bench.run("ls batch", [&]() {
+            auto [a, b] = Operon::LinearScalingCalculator::Calculate(xx, yy);
+            f += a + b;
 
-            b.run("correlation      ", [&]() { calc.Reset(); 
-                for (size_t i = 0; i < N; ++i) {
-                    calc.Add(x[i], y[i]);
-                }
-            });
-            b.run("correlation-simd", [&]() { calc.Reset(); calc.Add(x, y); });
+        });
 
-            SUBCASE("correlation")
-            {
-                nb::Bench b1;
-                b1.performanceCounters(true).relative(true).minEpochIterations(10);
-                b1.run("correlation      ", [&]() {
-                    calc.Reset();
-                    for (size_t i = 0; i < N; ++i) {
-                        calc.Add(x[i], y[i]);
-                    }
-                });
-            }
+        bench.run("ls online", [&]() {
+            auto [a, b] = Operon::LinearScalingCalculator::Calculate(xx.begin(), xx.end(), yy.begin());
+            f += a + b;
+        });
 
-            SUBCASE("correlation-simd")
-            {
-                nb::Bench b1;
-                b1.performanceCounters(true).relative(true).minEpochIterations(10);
-                b1.run("correlation-simd ", [&]() { calc.Reset(); calc.Add(x, y); });
-            }
-        }
     }
 }
 }
