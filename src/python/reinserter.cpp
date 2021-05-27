22e31fead7ca445b204421af819081c975551e44
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-05-27 18:34:02 +0200

    remove execution policy in reinserters, use pdqsort (pattern defeating quicksort)

--- src/python/reinserter.cpp
+++ src/python/reinserter.cpp
@@ -2,24 +2,25 @@
 // SPDX-FileCopyrightText: Copyright 2019-2021 Heal Research
 
 #include "operon.hpp"
-#include <operators/reinserter/keepbest.hpp>
+#include "operators/reinserter/keepbest.hpp"
+#include "operators/reinserter/replaceworst.hpp"
 
 void init_reinserter(py::module_ &m)
 {
     // reinserter
     py::class_<Operon::ReinserterBase>(m, "ReinserterBase");
 
-    py::class_<Operon::ReplaceWorstReinserter<std::execution::parallel_unsequenced_policy>, Operon::ReinserterBase>(m, "ReplaceWorstReinserter")
+    py::class_<Operon::ReplaceWorstReinserter, Operon::ReinserterBase>(m, "ReplaceWorstReinserter")
         .def(py::init([](size_t i) { 
             return Operon::ReplaceWorstReinserter([i](const auto& a, const auto& b) { return a[i] < b[i]; });
                     }), py::arg("objective_index"))
         .def(py::init<Operon::ComparisonCallback>())
-        .def("__call__", &Operon::ReplaceWorstReinserter<std::execution::parallel_unsequenced_policy>::operator());
+        .def("__call__", &Operon::ReplaceWorstReinserter::operator());
 
-    py::class_<Operon::KeepBestReinserter<std::execution::parallel_unsequenced_policy>, Operon::ReinserterBase>(m, "KeepBestReinserter")
+    py::class_<Operon::KeepBestReinserter, Operon::ReinserterBase>(m, "KeepBestReinserter")
         .def(py::init([](size_t i) {
             return Operon::KeepBestReinserter([i](const auto& a, const auto& b) { return a[i] < b[i]; });
                     }), py::arg("objective_index"))
         .def(py::init<Operon::ComparisonCallback>())
-        .def("__call__", &Operon::KeepBestReinserter<std::execution::parallel_unsequenced_policy>::operator());
+        .def("__call__", &Operon::KeepBestReinserter::operator());
 }
