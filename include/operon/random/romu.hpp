6eaa0b7696a263724c95ce9b6d4e666bb9d12baf
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-05-27 17:46:43 +0200

    add wyrand, allow move ctors in RNGs and disallow copy ctors

--- include/operon/random/romu.hpp
+++ include/operon/random/romu.hpp
@@ -32,34 +32,42 @@ namespace Random {
         static inline constexpr uint64_t(min)() { return result_type { 0 }; }
         static inline constexpr uint64_t(max)() { return std::numeric_limits<result_type>::max(); }
 
-        RomuTrio(RomuTrio const&) = delete;
-        RomuTrio& operator=(RomuTrio const&) = delete;
-
         explicit RomuTrio(uint64_t seed) noexcept
-            : xState(detail::splitMix64(seed))
-            , yState(detail::splitMix64(seed))
-            , zState(detail::splitMix64(seed))
+            : state{ detail::splitMix64(seed), detail::splitMix64(seed), detail::splitMix64(seed) }
         {
             for (size_t i = 0; i < 10; ++i) {
                 operator()();
             }
         }
 
+        // disallow copying (to prevent misuse)
+        RomuTrio(RomuTrio const&) = delete;
+        RomuTrio& operator=(RomuTrio const&) = delete;
+
+        // allow moving
+        RomuTrio(RomuTrio&&) noexcept = default;
+        RomuTrio& operator=(RomuTrio&&) noexcept = default;
+
+        ~RomuTrio() noexcept = default;
+
         inline uint64_t operator()() noexcept
         {
-            uint64_t xp = xState, yp = yState, zp = zState;
-            xState = 15241094284759029579u * zp;
-            yState = yp - xp;
-            yState = detail::rotl(yState, 12);
-            zState = zp - yp;
-            zState = detail::rotl(zState, 44);
+            uint64_t xp = state.x, yp = state.y, zp = state.z;
+            state.x = 15241094284759029579u * zp;
+            state.y = yp - xp;
+            state.y = detail::rotl(state.y, 12);
+            state.z = zp - yp;
+            state.z = detail::rotl(state.z, 44);
             return xp;
         }
 
+
     private:
-        uint64_t xState;
-        uint64_t yState;
-        uint64_t zState;
+        struct state {
+            uint64_t x;
+            uint64_t y;
+            uint64_t z;
+        } state;
     };
 
     class RomuDuo final {
@@ -69,29 +77,37 @@ namespace Random {
         static inline constexpr uint64_t(min)() { return result_type { 0 }; }
         static inline constexpr uint64_t(max)() { return std::numeric_limits<result_type>::max(); }
 
-        RomuDuo(RomuDuo const&) = delete;
-        RomuDuo& operator=(RomuDuo const&) = delete;
-
         explicit RomuDuo(uint64_t seed) noexcept
-            : xState(detail::splitMix64(seed))
-            , yState(detail::splitMix64(seed))
+            : state{ detail::splitMix64(seed), detail::splitMix64(seed) }
         {
             for (size_t i = 0; i < 10; ++i) {
                 operator()();
             }
         }
 
+        // disallow copying (to prevent misuse)
+        RomuDuo(RomuDuo const&) = delete;
+        RomuDuo& operator=(RomuDuo const&) = delete;
+
+        // allow moving
+        RomuDuo(RomuDuo&&) noexcept = default;
+        RomuDuo& operator=(RomuDuo&&) noexcept = default;
+
+        ~RomuDuo() noexcept = default;
+
         inline uint64_t operator()() noexcept
         {
-            uint64_t xp = xState;
-            xState = 15241094284759029579u * yState;
-            yState = detail::rotl(yState, 36) + detail::rotl(yState, 15) - xp;
+            uint64_t xp = state.x;
+            state.x = 15241094284759029579u * state.y;
+            state.y = detail::rotl(state.y, 36) + detail::rotl(state.y, 15) - xp;
             return xp;
         }
 
     private:
-        uint64_t xState;
-        uint64_t yState;
+        struct state {
+            uint64_t x;
+            uint64_t y;
+        } state;
     };
 
 }
