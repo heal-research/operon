09881ec2b5a7e79f694c8350fccedef839a6de7e
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-05-31 20:21:23 +0200

    improve struct distance performance

--- include/operon/core/distance.hpp
+++ include/operon/core/distance.hpp
@@ -4,7 +4,7 @@
 #ifndef OPERON_DISTANCE_HPP
 #define OPERON_DISTANCE_HPP
 
-#include "types.hpp"
+#include "core/types.hpp"
 
 #define VCL_NAMESPACE vcl
 #include "vectorclass.h"
@@ -16,60 +16,65 @@
 namespace Operon {
 namespace Distance {
     namespace {
-        template<typename T, std::enable_if_t<std::is_integral_v<T> && (sizeof(T) == 4 || sizeof(T) == 8), bool> = true>
-        constexpr bool Intersect(T const* lhs, T const* rhs) noexcept
+        template<typename T, std::enable_if_t<std::is_integral_v<T> && sizeof(T) == 8, bool> = true>
+        constexpr inline auto check(T const* lhs, T const* rhs) noexcept
         {
-            auto a = std::conditional_t<sizeof(T) == 4, vcl::Vec4ui, vcl::Vec4uq>().load(lhs);
-            return vcl::horizontal_add(a == rhs[0] | a == rhs[1] | a == rhs[2] | a == rhs[3]);
+            auto a = vcl::Vec4uq().load(lhs);
+            return a == rhs[0] | a == rhs[1] | a == rhs[2] | a == rhs[3];
+        }
+
+        template<typename T, std::enable_if_t<std::is_integral_v<T> && sizeof(T) == 4, bool> = true>
+        constexpr inline auto check(T const* lhs, T const* rhs) noexcept
+        {
+            auto a = vcl::Vec8ui().load(lhs);
+            return a == rhs[0] | a == rhs[1] | a == rhs[2] | a == rhs[3] | a == rhs[4] | a == rhs[5] | a == rhs[6] | a == rhs[7];
+        }
+
+        template<typename T, std::enable_if_t<std::is_integral_v<T> && (sizeof(T) == 4 || sizeof(T) == 8), bool> = true>
+        constexpr inline bool Intersect(T const* lhs, T const* rhs) noexcept
+        {
+            return vcl::horizontal_add(check(lhs, rhs));
         }
 
         // this method only works when the hash vectors are sorted
-        template<typename T>
-        size_t CountIntersect(Operon::Span<T> lhs, Operon::Span<T> rhs) noexcept
+        template<typename T, size_t S = size_t{32} / sizeof(T)>
+        inline size_t CountIntersect(Operon::Span<T> lhs, Operon::Span<T> rhs) noexcept
         {
-            size_t ls = lhs.size();
-            size_t rs = rhs.size();
+            T const *p0 = lhs.data(), *pS = p0 + (lhs.size() & (-S)), *pN = p0 + lhs.size();
+            T const *q0 = rhs.data(), *qS = q0 + (rhs.size() & (-S)), *qN = q0 + rhs.size();
+            T const *p = p0, *q = q0;
 
-            constexpr auto s = sizeof(T);
-            auto lt = ls & (-s);
-            auto rt = rs & (-s);
-
-            T const* p = lhs.data();
-            T const* q = rhs.data();
-            size_t count = 0;
-            size_t i = 0;
-            size_t j = 0;
-            while (i < lt && j < rt) {
-                if(Intersect(p + i, q + j)) {
+            while(p < pS && q < qS) {
+                if (Intersect(p, q)) {
                     break;
-                }
-                auto a = p[i + 3];
-                auto b = q[j + 3];
-                // we cannot have a == b because of !Intersect 
-                if (a < b) i += 4;
-                if (a > b) j += 4;
+                } 
+                auto const a = *(p + S - 1);
+                auto const b = *(q + S - 1);
+                if (a < b) p += S;
+                if (a > b) q += S;
             }
 
-            auto lm = lhs.back();
-            auto rm = rhs.back();
+            auto const aN = *(pN - 1);
+            auto const bN = *(qN - 1);
+            size_t count{0};
+            while(p < pN && q < qN) {
+                auto const a = *p;
+                auto const b = *q;
 
-            while (i < ls && j < rs) {
-                auto a = lhs[i];
-                auto b = rhs[j];
+                if (a > bN || b > aN) {
+                    break;
+                }
 
                 count += a == b;
-                i += a <= b;
-                j += b <= a;
-
-                if (a > rm || b > lm) {
-                    break;
-                }
+                p += a <= b;
+                q += a >= b;
             }
+
             return count;
         }
 
         template<typename Container>
-        size_t CountIntersect(Container const& lhs, Container const& rhs) noexcept
+        inline size_t CountIntersect(Container const& lhs, Container const& rhs) noexcept
         {
             using T = typename Container::value_type;
             return CountIntersect(Operon::Span<T const>(lhs.data(), lhs.size()), Operon::Span<T const>(rhs.data(), rhs.size()));
