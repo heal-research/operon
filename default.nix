2dc07cfae1dcdfb4e2f3a1453a419fccc7363395
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-09-03 14:57:57 +0200

    clean up nix env

--- default.nix
+++ default.nix
@@ -3,54 +3,15 @@
 
 let
   pkgs = import ~/nix/nixpkgs {
-      #localSystem = {
-      #  gcc.arch = "znver2";
-      #  gcc.tune = "znver2";
-      #  system = "x86_64-linux";
-      #};
   };
-
-  eigen = pkgs.eigen.overrideAttrs (old: rec {
-    version = "3.4";
-    stdenv = pkgs.gcc11Stdenv;
-    src = pkgs.fetchFromGitLab {
-      repo = "eigen";
-      owner = "libeigen";
-      rev = "7b35638ddb99a0298c5d3450de506a8e8e0203d3";
-      sha256 = "sha256:1q54l3g6za6jf0iaq073bqmc9gc5w14bliacgk4zysnhk44fla39";
-    };
-    patches = [];
-    cmakeFlags = [ "-DCMAKE_PREFIX_PATH=$out" "-DINCLUDE_INSTALL_DIR=include/eigen3" ];
-  });
-
-  ceres-solver = pkgs.ceres-solver.overrideAttrs (old: rec {
-    CFLAGS = (old.CFLAGS or "") + "-march=native -O3";
-    stdenv = pkgs.gcc11Stdenv;
-    buildInputs = with pkgs; [ eigen glog ];
-
-    version = "2.0.0";
-    src = pkgs.fetchFromGitHub {
-      repo   = "ceres-solver";
-      owner  = "ceres-solver";
-      rev    = "c036c78196c7a9f36e48e6387691e8f4979aef5d";
-      sha256 = "sha256-8YwC9lvlZe0notDUyCxvFEn185Sj4s9Rmkt82h4Xqbc=";
-    };
-    enableParallelBuilding = true;
-    cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" "-DCXX11=ON" "-DTBB=OFF" "-DOPENMP=OFF" "-DBUILD_SHARED_LIBS=OFF" "-DBUILD_EXAMPLES=FALSE" "-DBUILD_TESTING=FALSE" ];
-  });
-
-  fmt = pkgs.fmt.overrideAttrs(old: rec { 
-    outputs = [ "out" ];
-    cmakeFlags = [ "-DBUILD_SHARED_LIBS=ON" "-DFMT_TEST=OFF" "-DFMT_CUDA_TEST=OFF" "-DFMT_FUZZ=OFF" ];
-  });
 in
   pkgs.gcc11Stdenv.mkDerivation {
     name = "operon-env";
-    hardeningDisable = [ "all" ]; 
+    hardeningDisable = [ "all" ];
 
     impureUseNativeOptimizations = true;
 
-    nativeBuildInputs = with pkgs; [ bear cmake clang_12 clang-tools ];
+    nativeBuildInputs = with pkgs; [ cmake clang_12 clang-tools ];
 
     buildInputs = with pkgs; [
         # python environment for bindings and scripting
@@ -66,8 +27,6 @@ in
         fmt
         gdb
         glog
-        gperftools
-        graphviz
         hyperfine
         jemalloc
         linuxPackages.perf
