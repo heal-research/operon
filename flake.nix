fc7b75df9bbcdae4a7a6e8bcc25335e5fab3b744
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-10-25 09:51:25 +0200

    update nix files

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Operon development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
