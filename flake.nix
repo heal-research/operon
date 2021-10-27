507c107e0761ff4e5e202c897bbb272e243b0149
Bogdan Burlacu <bogdan.burlacu@pm.me>
2021-10-27 15:53:24 +0000

    update flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,50 @@
 {
   description = "Operon development environment";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nur.url = "github:nix-community/NUR";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, flake-utils, nixpkgs, nur }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ nur.overlay ];
+          };
+        in
         {
-          devShell = import ./shell.nix { inherit pkgs; };
+          devShell = pkgs.gcc11Stdenv.mkDerivation {
+            name = "operon-env";
+            hardeningDisable = [ "all" ];
+            impureUseNativeOptimizations = true;
+            nativeBuildInputs = with pkgs; [ cmake clang_12 clang-tools ];
+            buildInputs = with pkgs; [
+                # python environment for bindings and scripting
+                (python39.withPackages (ps: with ps; [ pybind11 pytest pip numpy scipy scikitlearn pandas sympy pyperf colorama coloredlogs seaborn cython jupyterlab ipywidgets grip livereload joblib graphviz sphinx recommonmark sphinx_rtd_theme ]))
+                # Project dependencies and utils for profiling and debugging
+                pkgs.nur.repos.foolnotion.eli5
+                pkgs.nur.repos.foolnotion.pmlb
+                pkgs.nur.repos.foolnotion.taskflow
+                ceres-solver
+                cmake
+                cxxopts
+                diff-so-fancy
+                doctest
+                eigen
+                fmt
+                gdb
+                glog
+                hyperfine
+                jemalloc
+                linuxPackages.perf
+                mimalloc
+                ninja
+                openlibm
+                valgrind
+              ];
+          };
         }
       );
 }
