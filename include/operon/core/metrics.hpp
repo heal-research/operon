cef7fb40f7663799dc2f93bd2ed3d1e87ed41a5f
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-01-09 12:23:51 +0100

    disambiguate metrics (r2 vs squared corr)

--- include/operon/core/metrics.hpp
+++ include/operon/core/metrics.hpp
@@ -12,20 +12,26 @@
 
 namespace Operon {
 
-//template<typename T>
-//inline auto SquaredError(T a, T b) -> T
-//{
-//    auto e = a - b; return e * e;
-//}
-const auto SquaredError = [](auto a, auto b) { auto e = a - b; return e * e; };
+inline const auto SquaredError = [](auto a, auto b) { auto e = a - b; return e * e; };
 
-template<typename T>
-inline auto MeanSquaredError(Operon::Span<const T> x, Operon::Span<const T> y) noexcept -> double
+template<typename InputIt1, typename InputIt2>
+inline auto CoefficientOfDetermination(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
 {
-    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
-    EXPECT(x.size() == y.size());
-    EXPECT(x.size() > 0);
-    return univariate::accumulate<T>(x.data(), y.data(), x.size(), SquaredError).mean;
+    using V1 = typename std::iterator_traits<InputIt1>::value_type;
+    using V2 = typename std::iterator_traits<InputIt2>::value_type;
+    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
+    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
+    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
+    constexpr double eps{1e-12};
+    auto ssr = univariate::accumulate<V1>(begin1, end1, begin2, SquaredError).sum;
+    auto end2 = begin2;
+    std::advance(end2, std::distance(begin1, end1));
+    auto meanY = univariate::accumulate<V2>(begin2, end2).mean;
+    auto sst = univariate::accumulate<V2>(begin2, end2, [&](auto v) { return SquaredError(v, meanY);} ).sum;
+    if (sst < eps) {
+        return std::numeric_limits<double>::min();
+    }
+    return 1.0 - ssr / sst;
 }
 
 template<typename InputIt1, typename InputIt2>
@@ -39,27 +45,10 @@ inline auto MeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) no
     return univariate::accumulate<V1>(begin1, end1, begin2, SquaredError).mean;
 }
 
-template<typename T>
-inline auto RootMeanSquaredError(Operon::Span<const T> x, Operon::Span<const T> y) noexcept -> double
-{
-    return std::sqrt(MeanSquaredError(x, y));
-}
-
 template<typename InputIt1, typename InputIt2>
 inline auto RootMeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
 {
     return std::sqrt(MeanSquaredError(begin1, end1, begin2));
-}
-
-template<typename T>
-inline auto NormalizedMeanSquaredError(Operon::Span<const T> x, Operon::Span<const T> y) noexcept -> double
-{
-    constexpr double eps{1e-12};
-    auto varY = univariate::accumulate<T>(y.begin(), y.end()).variance;
-    if (std::abs(varY) < eps) {
-        return varY;
-    }
-    return MeanSquaredError(x, y) / varY;
 }
 
 template<typename InputIt1, typename InputIt2>
@@ -79,15 +68,27 @@ inline auto NormalizedMeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 
     return MeanSquaredError(begin1, end1, begin2) / varY;
 }
 
-template<typename T>
-inline auto L2Norm(Operon::Span<const T> x, Operon::Span<const T> y) -> double
+template<typename InputIt1, typename InputIt2>
+inline auto MeanAbsoluteError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
 {
-    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
-    EXPECT(x.size() == y.size());
-    EXPECT(x.size() > 0);
-    Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>> a(x.data(), x.size());
-    Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>> b(y.data(), y.size());
-    return static_cast<double>((a - b).squaredNorm() / 2);
+    using V1 = typename std::iterator_traits<InputIt1>::value_type;
+    using V2 = typename std::iterator_traits<InputIt2>::value_type;
+    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
+    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
+    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
+    return univariate::accumulate<V1>(begin1, end1, begin2, [](auto a, auto b) { return std::abs(a-b); }).mean;
+}
+
+template<typename InputIt1, typename InputIt2>
+inline auto SquaredCorrelation(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
+{
+    using V1 = typename std::iterator_traits<InputIt1>::value_type;
+    using V2 = typename std::iterator_traits<InputIt2>::value_type;
+    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
+    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
+    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
+    auto r = bivariate::accumulate<V1>(begin1, end1, begin2).correlation;
+    return r * r;
 }
 
 template<typename InputIt1, typename InputIt2>
@@ -104,7 +105,35 @@ inline auto L2Norm(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> 
 }
 
 template<typename T>
-inline auto MeanAbsoluteError(Operon::Span<const T> x, Operon::Span<const T> y) -> double
+inline auto CoefficientOfDetermination(Operon::Span<T const> x, Operon::Span<T const> y) noexcept -> double
+{
+    EXPECT(y.size() == x.size());
+    return CoefficientOfDetermination(x.begin(), x.end(), y.begin());
+}
+
+template<typename T>
+inline auto MeanSquaredError(Operon::Span<T const> x, Operon::Span<T const> y) noexcept -> double
+{
+    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
+    EXPECT(x.size() == y.size());
+    EXPECT(x.size() > 0);
+    return univariate::accumulate<T>(x.data(), y.data(), x.size(), SquaredError).mean;
+}
+
+template<typename T>
+inline auto RootMeanSquaredError(Operon::Span<T const> x, Operon::Span<T const> y) noexcept -> double
+{
+    return std::sqrt(MeanSquaredError(x, y));
+}
+
+template<typename T>
+inline auto NormalizedMeanSquaredError(Operon::Span<T const> x, Operon::Span<T const> y) noexcept -> double
+{
+    return NormalizedMeanSquaredError(x.begin(), x.end(), y.begin());
+}
+
+template<typename T>
+inline auto MeanAbsoluteError(Operon::Span<T const> x, Operon::Span<T const> y) -> double
 {
     static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
     EXPECT(x.size() == y.size());
@@ -112,19 +141,19 @@ inline auto MeanAbsoluteError(Operon::Span<const T> x, Operon::Span<const T> y) 
     return univariate::accumulate<T>(x.data(), y.data(), x.size(), [](auto a, auto b) { return std::abs(a-b); }).mean;
 }
 
-template<typename InputIt1, typename InputIt2>
-inline auto MeanAbsoluteError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
+template<typename T>
+inline auto L2Norm(Operon::Span<T const> x, Operon::Span<T const> y) -> double
 {
-    using V1 = typename std::iterator_traits<InputIt1>::value_type;
-    using V2 = typename std::iterator_traits<InputIt2>::value_type;
-    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
-    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
-    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
-    return univariate::accumulate<V1>(begin1, end1, begin2, [](auto a, auto b) { return std::abs(a-b); }).mean;
+    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
+    EXPECT(x.size() == y.size());
+    EXPECT(x.size() > 0);
+    Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>> a(x.data(), x.size());
+    Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>> b(y.data(), y.size());
+    return static_cast<double>((a - b).squaredNorm() / 2);
 }
 
 template<typename T>
-inline auto RSquared(Operon::Span<const T> x, Operon::Span<const T> y) -> double
+inline auto SquaredCorrelation(Operon::Span<T const> x, Operon::Span<T const> y) -> double
 {
     static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
     EXPECT(x.size() == y.size());
@@ -133,22 +162,10 @@ inline auto RSquared(Operon::Span<const T> x, Operon::Span<const T> y) -> double
     return r * r;
 }
 
-template<typename InputIt1, typename InputIt2>
-inline auto RSquared(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
-{
-    using V1 = typename std::iterator_traits<InputIt1>::value_type;
-    using V2 = typename std::iterator_traits<InputIt2>::value_type;
-    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
-    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
-    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
-    auto r = bivariate::accumulate<V1>(begin1, end1, begin2).correlation;
-    return r * r;
-}
-
 // functors to plug into an evaluator
 struct MSE {
     template<typename T>
-    inline auto operator()(Operon::Span<const T> x, Operon::Span<const T> y) const noexcept -> double
+    inline auto operator()(Operon::Span<T const> x, Operon::Span<T const> y) const noexcept -> double
     {
         return MeanSquaredError(x, y);
     }
@@ -162,7 +179,7 @@ struct MSE {
 
 struct NMSE {
     template<typename T>
-    inline auto operator()(Operon::Span<const T> x, Operon::Span<const T> y) const noexcept -> double
+    inline auto operator()(Operon::Span<T const> x, Operon::Span<T const> y) const noexcept -> double
     {
         return NormalizedMeanSquaredError(x, y);
     }
@@ -176,7 +193,7 @@ struct NMSE {
 
 struct RMSE {
     template<typename T>
-    inline auto operator()(Operon::Span<const T> x, Operon::Span<const T> y) const noexcept -> double
+    inline auto operator()(Operon::Span<T const> x, Operon::Span<T const> y) const noexcept -> double
     {
         return RootMeanSquaredError(x, y);
     }
@@ -190,7 +207,7 @@ struct RMSE {
 
 struct MAE {
     template<typename T>
-    inline auto operator()(Operon::Span<const T> x, Operon::Span<const T> y) const noexcept -> double
+    inline auto operator()(Operon::Span<T const> x, Operon::Span<T const> y) const noexcept -> double
     {
         return MeanAbsoluteError(x, y);
     }
@@ -202,17 +219,17 @@ struct MAE {
     }
 };
 
-struct R2 {
+struct C2 {
     template<typename T>
-    inline auto operator()(Operon::Span<const T> x, Operon::Span<const T> y) const noexcept -> double
+    inline auto operator()(Operon::Span<T const> x, Operon::Span<T const> y) const noexcept -> double
     {
-        return -RSquared(x, y);
+        return -SquaredCorrelation(x, y);
     }
 
     template<typename InputIt1, typename InputIt2>
     inline auto operator()(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) const noexcept -> double
     {
-        return -RSquared(begin1, end1, begin2);
+        return -SquaredCorrelation(begin1, end1, begin2);
     }
 };
 
@@ -227,6 +244,20 @@ struct L2 {
     inline auto operator()(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) const noexcept -> double
     {
         return L2Norm(begin1, end1, begin2);
+    }
+};
+
+struct R2 {
+    template<typename T>
+    inline auto operator()(Operon::Span<T const> x, Operon::Span<T const> y) const noexcept -> double
+    {
+        return -CoefficientOfDetermination(x, y);
+    }
+
+    template<typename InputIt1, typename InputIt2>
+    inline auto operator()(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) const noexcept -> double
+    {
+        return -CoefficientOfDetermination(begin1, end1, begin2);
     }
 };
 
