9b47837043ff36a38be7405507997ea716c7aca3
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-01-17 11:00:35 +0100

    add individual error metrics

---
+++ include/operon/error_metrics/correlation_coefficient.hpp
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright 2019-2022 Heal Research
+
+#ifndef OPERON_METRICS_CORRELATION_COEFFICIENT_HPP
+#define OPERON_METRICS_CORRELATION_COEFFICIENT_HPP
+
+#include <iterator>
+#include <type_traits>
+#include <vstat/vstat.hpp>
+#include "operon/core/types.hpp"
+
+namespace Operon {
+
+template<typename InputIt1, typename InputIt2>
+inline auto CorrelationCoefficient(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
+{
+    using V1 = typename std::iterator_traits<InputIt1>::value_type;
+    using V2 = typename std::iterator_traits<InputIt2>::value_type;
+    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
+    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
+    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
+    auto r = bivariate::accumulate<V1>(begin1, end1, begin2).correlation;
+    return r * r;
+}
+
+template<typename T>
+inline auto CorrelationCoefficient(Operon::Span<T const> x, Operon::Span<T const> y) -> double
+{
+    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
+    EXPECT(x.size() == y.size());
+    EXPECT(x.size() > 0);
+    auto r = bivariate::accumulate<T>(x.data(), y.data(), x.size()).correlation;
+    return r * r;
+}
+} // namespace Operon
+
+#endif
---
+++ include/operon/error_metrics/r2_score.hpp
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright 2019-2022 Heal Research
+
+#ifndef OPERON_METRICS_R2_SCORE_HPP
+#define OPERON_METRICS_R2_SCORE_HPP
+
+#include <iterator>
+#include <type_traits>
+#include <vstat/vstat.hpp>
+#include "operon/core/types.hpp"
+
+namespace Operon {
+template<typename InputIt1, typename InputIt2>
+inline auto R2Score(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
+{
+    using V1 = typename std::iterator_traits<InputIt1>::value_type;
+    using V2 = typename std::iterator_traits<InputIt2>::value_type;
+    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
+    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
+    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
+    constexpr double eps{1e-12};
+    auto ssr = univariate::accumulate<V1>(begin1, end1, begin2, [](auto a, auto b) { auto e = a - b; return e * e; }).sum;
+    auto end2 = begin2;
+    std::advance(end2, std::distance(begin1, end1));
+    auto meanY = univariate::accumulate<V2>(begin2, end2).mean;
+    auto sst = univariate::accumulate<V2>(begin2, end2, [&](auto v) { auto e = v - meanY; return e * e;} ).sum;
+    if (sst < eps) {
+        return std::numeric_limits<double>::min();
+    }
+    return 1.0 - ssr / sst;
+}
+
+template<typename T>
+inline auto R2Score(Operon::Span<T const> x, Operon::Span<T const> y) noexcept -> double
+{
+    EXPECT(y.size() == x.size());
+    return R2Score(x.begin(), x.end(), y.begin());
+}
+} // namespace Operon
+
+#endif
