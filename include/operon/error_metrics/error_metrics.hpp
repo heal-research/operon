13aa279c772b60f5a44d129551c5af53404eddef
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-01-17 11:00:35 +0100

    add individual error metrics

---
+++ include/operon/error_metrics/error_metrics.hpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright 2019-2022 Heal Research
+
+#ifndef OPERON_ERROR_METRICS_HPP
+#define OPERON_ERROR_METRICS_HPP
+
+#include "operon/error_metrics/mean_squared_error.hpp"
+#include "operon/error_metrics/normalized_mean_squared_error.hpp"
+#include "operon/error_metrics/r2_score.hpp"
+#include "operon/error_metrics/correlation_coefficient.hpp"
+#include "operon/error_metrics/mean_absolute_error.hpp"
+
+#endif
+
