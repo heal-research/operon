afb5469254ccfe3718e252142172dff7357dba12
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-01-17 11:00:35 +0100

    add individual error metrics

---
+++ include/operon/error_metrics/mean_squared_error.hpp
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright 2019-2022 Heal Research
+
+#ifndef OPERON_METRICS_MEAN_SQUARED_ERROR_HPP
+#define OPERON_METRICS_MEAN_SQUARED_ERROR_HPP
+
+#include <iterator>
+#include <type_traits>
+#include <vstat/vstat.hpp>
+#include "operon/core/types.hpp"
+
+namespace Operon {
+
+template<typename InputIt1, typename InputIt2>
+inline auto MeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
+{
+    using V1 = typename std::iterator_traits<InputIt1>::value_type;
+    using V2 = typename std::iterator_traits<InputIt2>::value_type;
+    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
+    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
+    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
+    return univariate::accumulate<V1>(begin1, end1, begin2, [](auto a, auto b) { auto e = a - b; return e * e; }).mean;
+}
+
+template<typename InputIt1, typename InputIt2>
+inline auto RootMeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
+{
+    return std::sqrt(MeanSquaredError(begin1, end1, begin2));
+}
+
+template<typename T>
+inline auto MeanSquaredError(Operon::Span<T const> x, Operon::Span<T const> y) noexcept -> double
+{
+    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
+    EXPECT(x.size() == y.size());
+    EXPECT(x.size() > 0);
+    return univariate::accumulate<T>(x.data(), y.data(), x.size(), [](auto a, auto b) { auto e = a - b; return e * e; }).mean;
+}
+
+template<typename T>
+inline auto RootMeanSquaredError(Operon::Span<T const> x, Operon::Span<T const> y) noexcept -> double
+{
+    return std::sqrt(MeanSquaredError(x, y));
+}
+
+} // namespace Operon
+
+#endif
---
+++ include/operon/error_metrics/normalized_mean_squared_error.hpp
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright 2019-2022 Heal Research
+
+#ifndef OPERON_METRICS_NORMALIZED_MEAN_SQUARED_ERROR_HPP
+#define OPERON_METRICS_NORMALIZED_MEAN_SQUARED_ERROR_HPP
+
+#include <iterator>
+#include <type_traits>
+#include <vstat/vstat.hpp>
+#include "operon/core/types.hpp"
+#include "mean_squared_error.hpp"
+
+namespace Operon {
+
+template<typename InputIt1, typename InputIt2>
+inline auto NormalizedMeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
+{
+    using V1 = typename std::iterator_traits<InputIt1>::value_type;
+    using V2 = typename std::iterator_traits<InputIt2>::value_type;
+    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
+    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
+    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
+
+    constexpr double eps{1e-12};
+    auto varY = univariate::accumulate<V1>(begin2, begin2 + std::distance(begin1, end1)).variance;
+    if (std::abs(varY) < eps) {
+        return varY;
+    }
+    return MeanSquaredError(begin1, end1, begin2) / varY;
+}
+
+template<typename T>
+inline auto NormalizedMeanSquaredError(Operon::Span<T const> x, Operon::Span<T const> y) noexcept -> double
+{
+    return NormalizedMeanSquaredError(x.begin(), x.end(), y.begin());
+}
+} // namespace Operon
+
+#endif
