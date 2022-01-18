87c12832985247eac337aca30924c4db416fd4e7
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-01-19 00:49:04 +0100

    minor refactor

--- cli/source/operator_factory.hpp
+++ cli/source/operator_factory.hpp
@@ -11,8 +11,8 @@
 #include <utility>                             // for addressof
 #include <vector>                              // for vector
 #include "operon/core/types.hpp"               // for Span
+#include "operon/core/individual.hpp"          // for Comparison
 #include "operon/interpreter/interpreter.hpp"  // for Interpreter
-#include "operon/operators/selector.hpp"       // for SelectorBase, Proporti...
 #include "util.hpp"                            // for Split
 namespace Operon { class EvaluatorBase; }
 namespace Operon { class KeepBestReinserter; }
@@ -21,6 +21,7 @@ namespace Operon { class PrimitiveSet; }
 namespace Operon { class Problem; }
 namespace Operon { class ReinserterBase; }
 namespace Operon { class ReplaceWorstReinserter; }
+namespace Operon { class SelectorBase; }
 namespace Operon { struct CreatorBase; }
 namespace Operon { struct CrossoverBase; }
 namespace Operon { struct ErrorMetric; }
@@ -29,44 +30,9 @@ namespace Operon { struct Variable; }
 
 namespace Operon {
 
-template<typename Comp>
-auto ParseReinserter(std::string const& str, Comp&& comp)
-{
-    std::unique_ptr<ReinserterBase> reinserter;
-    if (str == "keep-best") {
-        reinserter = std::make_unique<KeepBestReinserter>(comp);
-    } else if (str == "replace-worst") {
-        reinserter = std::make_unique<ReplaceWorstReinserter>(comp);
-    }
-    return reinserter;
-}
+auto ParseReinserter(std::string const& str, ComparisonCallback&& comp) -> std::unique_ptr<ReinserterBase>;
 
-template<typename Comp>
-auto ParseSelector(std::string const& str, Comp&& comp) -> std::unique_ptr<Operon::SelectorBase>
-{
-    auto tok = Split(str, ':');
-    auto name = tok[0];
-    std::unique_ptr<Operon::SelectorBase> selector;
-    constexpr size_t defaultTournamentSize{5};
-    if (name == "tournament") {
-        selector = std::make_unique<Operon::TournamentSelector>(comp);
-        size_t tournamentSize{defaultTournamentSize};
-        if (tok.size() > 1) { scn::scan(tok[1], "{}", tournamentSize); }
-        dynamic_cast<Operon::TournamentSelector*>(selector.get())->SetTournamentSize(tournamentSize);
-    } else if (name == "proportional") {
-        selector = std::make_unique<Operon::ProportionalSelector>(comp);
-        dynamic_cast<Operon::ProportionalSelector*>(selector.get())->SetObjIndex(0);
-    } else if (name == "rank") {
-        selector = std::make_unique<Operon::RankTournamentSelector>(comp);
-        size_t tournamentSize{defaultTournamentSize};
-        if (tok.size() > 1) { scn::scan(tok[1], "{}", tournamentSize); }
-        dynamic_cast<Operon::RankTournamentSelector*>(selector.get())->SetTournamentSize(tournamentSize);
-    } else if (name == "random") {
-        selector = std::make_unique<Operon::RandomSelector>();
-    }
-        
-    return selector;
-}
+auto ParseSelector(std::string const& str, ComparisonCallback&& comp) -> std::unique_ptr<SelectorBase>;
 
 auto ParseCreator(std::string const& str, PrimitiveSet const& pset, Operon::Span<Variable const> inputs) -> std::unique_ptr<CreatorBase>;
 
