9da485242874abe833328665478a358241656b57
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-01-20 13:53:02 +0100

    simplify cmake options

--- flake.nix
+++ flake.nix
@@ -6,57 +6,78 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
 
   outputs = { self, flake-utils, nixpkgs, nur }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ nur.overlay ];
-          };
-          repo = pkgs.nur.repos.foolnotion;
-        in rec
-        {
-          defaultPackage = pkgs.gcc11Stdenv.mkDerivation {
-            name = "operon";
-            src = self;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nur.overlay ];
+        };
+        repo = pkgs.nur.repos.foolnotion;
+      in rec {
+        defaultPackage = pkgs.gcc11Stdenv.mkDerivation {
+          name = "operon";
+          src = self;
 
-            cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" "-DUSE_SINGLE_PRECISION=ON" "-DBUILD_CLI_PROGRAMS=ON" "-DBUILD_TESTING=OFF" "-DBUILD_SHARED_LIBS=ON" ];
+          cmakeFlags = [
+            "-DCMAKE_BUILD_TYPE=Release"
+            "-DUSE_SINGLE_PRECISION=ON"
+            "-DBUILD_CLI_PROGRAMS=ON"
+            "-DBUILD_TESTING=OFF"
+            "-DBUILD_SHARED_LIBS=ON"
+          ];
 
-            nativeBuildInputs = with pkgs; [ cmake ];
+          nativeBuildInputs = with pkgs; [ cmake ];
 
-            buildInputs = with pkgs; [
-              ceres-solver
-              cxxopts
-              doctest
-              eigen
-              fmt
-              git
-              glog
-              # Some dependencies are provided by a NUR repo
-              repo.aria-csv
-              repo.fast_float
-              repo.pratt-parser
-              repo.robin-hood-hashing
-              repo.scnlib
-              repo.span-lite
-              repo.taskflow
-              repo.vectorclass
-              repo.vstat
-              repo.xxhash
-            ];
-          };
+          buildInputs = with pkgs; [
+            ceres-solver
+            cxxopts
+            doctest
+            eigen
+            fmt
+            git
+            glog
+            jemalloc
+            openlibm
+            pkg-config
+            # Some dependencies are provided by a NUR repo
+            repo.aria-csv
+            repo.fast_float
+            repo.pratt-parser
+            repo.robin-hood-hashing
+            repo.scnlib
+            repo.span-lite
+            repo.taskflow
+            repo.vectorclass
+            repo.vstat
+            repo.xxhash
+          ];
+        };
 
-          devShell = pkgs.gcc11Stdenv.mkDerivation {
-            name = "operon-env";
-            hardeningDisable = [ "all" ];
-            impureUseNativeOptimizations = true;
-            nativeBuildInputs = with pkgs; [ bear cmake clang_13 clang-tools cppcheck include-what-you-use ];
-            buildInputs = defaultPackage.buildInputs ++ (with pkgs; [ gdb hotspot valgrind jemalloc linuxPackages.perf ]);
+        devShell = pkgs.gcc11Stdenv.mkDerivation {
+          name = "operon-env";
+          hardeningDisable = [ "all" ];
+          impureUseNativeOptimizations = true;
+          nativeBuildInputs = with pkgs; [
+            bear
+            cmake
+            clang_13
+            clang-tools
+            cppcheck
+            include-what-you-use
+          ];
+          buildInputs = defaultPackage.buildInputs ++ (with pkgs; [
+            gdb
+            hotspot
+            valgrind
+            jemalloc
+            linuxPackages.perf
+          ]);
 
-            shellHook = ''
-              LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.gcc11Stdenv.cc.cc.lib ]};
-              '';
-          };
-        }
-      );
+          shellHook = ''
+            LD_LIBRARY_PATH=${
+              pkgs.lib.makeLibraryPath [ pkgs.gcc11Stdenv.cc.cc.lib ]
+            };
+          '';
+        };
+      });
 }
