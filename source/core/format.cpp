04df8ed82a9aaac5876207353eca0dac4516b5cd
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-02-26 18:25:23 +0100

    update due to deprecations in fmt api

--- source/core/format.cpp
+++ source/core/format.cpp
@@ -72,77 +72,77 @@ void InfixFormatter::FormatNode(Tree const& tree, std::unordered_map<Operon::Has
     const auto& s = tree[i];
     if (s.IsConstant()) {
         auto formatString = fmt::format(s.Value < 0 ? "({{:.{}f}})" : "{{:.{}f}}", decimalPrecision);
-        fmt::format_to(current, formatString, s.Value);
+        fmt::format_to(std::back_inserter(current), formatString, s.Value);
     } else if (s.IsVariable()) {
         auto formatString = fmt::format(s.Value < 0 ? "(({{:.{}f}}) * {{}})" : "({{:.{}f}} * {{}})", decimalPrecision);
         if (auto it = variableNames.find(s.CalculatedHashValue); it != variableNames.end()) {
-            fmt::format_to(current, formatString, s.Value, it->second);
+            fmt::format_to(std::back_inserter(current), formatString, s.Value, it->second);
         } else {
             throw std::runtime_error(fmt::format("A variable with hash value {} could not be found in the dataset.\n", s.CalculatedHashValue));
         }
     } else {
         if (s.Type < NodeType::Abs) // add, sub, mul, div, aq, fmax, fmin, pow
         {
-            fmt::format_to(current, "(");
+            fmt::format_to(std::back_inserter(current), "(");
             if (s.Arity == 1) {
                 if (s.Type == NodeType::Sub) {
                     // subtraction with a single argument is a negation -x
-                    fmt::format_to(current, "-");
+                    fmt::format_to(std::back_inserter(current), "-");
                 } else if (s.Type == NodeType::Div) {
                     // division with a single argument is an inversion 1/x
-                    fmt::format_to(current, "1 / ");
+                    fmt::format_to(std::back_inserter(current), "1 / ");
                 }
             } else if (s.Type == NodeType::Pow) {
                 // format pow(a,b) as a^b
                 auto j = i - 1;
                 auto k = j - tree[j].Length - 1;
                 FormatNode(tree, variableNames, j, current, decimalPrecision);
-                fmt::format_to(current, " ^ ");
+                fmt::format_to(std::back_inserter(current), " ^ ");
                 FormatNode(tree, variableNames, k, current, decimalPrecision);
             } else if (s.Type == NodeType::Aq) {
                 // format aq(a,b) as a / (1 + b^2)
                 auto j = i - 1;
                 auto k = j - tree[j].Length - 1;
                 FormatNode(tree, variableNames, j, current, decimalPrecision);
-                fmt::format_to(current, " / (sqrt(1 + ");
+                fmt::format_to(std::back_inserter(current), " / (sqrt(1 + ");
                 FormatNode(tree, variableNames, k, current, decimalPrecision);
-                fmt::format_to(current, " ^ 2))");
+                fmt::format_to(std::back_inserter(current), " ^ 2))");
             } else {
                 size_t count = 0;
                 for (auto it = tree.Children(i); it.HasNext(); ++it) {
                     FormatNode(tree, variableNames, it.Index(), current, decimalPrecision);
                     if (++count < s.Arity) {
-                        fmt::format_to(current, " {} ", s.Name());
+                        fmt::format_to(std::back_inserter(current), " {} ", s.Name());
                     }
                 }
             }
-            fmt::format_to(current, ")");
+            fmt::format_to(std::back_inserter(current), ")");
         } else { // unary operators abs, asin, ... log, exp, sin, etc.
             if (s.Type == NodeType::Square) {
                 // format square(a) as a ^ 2
-                fmt::format_to(current, "(");
+                fmt::format_to(std::back_inserter(current), "(");
                 FormatNode(tree, variableNames, i - 1, current, decimalPrecision);
-                fmt::format_to(current, " ^ 2)");
+                fmt::format_to(std::back_inserter(current), " ^ 2)");
             } else if (s.Type == NodeType::Logabs) {
                 // format logabs(a) as log(abs(a))
-                fmt::format_to(current, "log(abs(");
+                fmt::format_to(std::back_inserter(current), "log(abs(");
                 FormatNode(tree, variableNames, i - 1, current, decimalPrecision);
-                fmt::format_to(current, "))");
+                fmt::format_to(std::back_inserter(current), "))");
             } else if (s.Type == NodeType::Log1p) {
                 // format log1p(a) as log(a+1)
-                fmt::format_to(current, "log(");
+                fmt::format_to(std::back_inserter(current), "log(");
                 FormatNode(tree, variableNames, i - 1, current, decimalPrecision);
-                fmt::format_to(current, "+1)");
+                fmt::format_to(std::back_inserter(current), "+1)");
             } else if (s.Type == NodeType::Sqrtabs) {
                 // format sqrtabs(a) as sqrt(abs(a))
-                fmt::format_to(current, "sqrt(abs(");
+                fmt::format_to(std::back_inserter(current), "sqrt(abs(");
                 FormatNode(tree, variableNames, i - 1, current, decimalPrecision);
-                fmt::format_to(current, "))");
+                fmt::format_to(std::back_inserter(current), "))");
             } else {
-                fmt::format_to(current, "{}", s.Name());
-                fmt::format_to(current, "(");
+                fmt::format_to(std::back_inserter(current), "{}", s.Name());
+                fmt::format_to(std::back_inserter(current), "(");
                 FormatNode(tree, variableNames, i - 1, current, decimalPrecision);
-                fmt::format_to(current, ")");
+                fmt::format_to(std::back_inserter(current), ")");
             }
         }
     }
