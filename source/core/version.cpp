2e9b1a1a42749917eeffbf9f37e9e3e972260f35
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-03-08 08:35:51 +0100

    fix warnings in version.cpp

--- source/core/version.cpp
+++ source/core/version.cpp
@@ -7,26 +7,20 @@
 namespace Operon {
 
 auto Version() -> std::string {
-#if defined(CERES_TINY_SOLVER)
-        constexpr auto solver_name = "tiny";
-#else
-        constexpr auto solver_name = "ceres";
-#endif
-
 #if defined(USE_SINGLE_PRECISION)
         constexpr auto precision = "single";
 #else
         constexpr auto precision = "double";
 #endif
         fmt::memory_buffer buf;// NOLINT
-        fmt::format_to(buf, "operon rev. {} {} {} {}, timestamp {}\n", OPERON_REVISION, OPERON_BUILD, OPERON_PLATFORM, OPERON_ARCH, OPERON_BUILD_TIMESTAMP);// NOLINT
-        fmt::format_to(buf, "{}-precision build using eigen {}" , precision, OPERON_EIGEN_VERSION);// NOLINT
+        fmt::format_to(std::back_inserter(buf), "operon rev. {} {} {} {}, timestamp {}\n", OPERON_REVISION, OPERON_BUILD, OPERON_PLATFORM, OPERON_ARCH, OPERON_BUILD_TIMESTAMP);// NOLINT
+        fmt::format_to(std::back_inserter(buf), "{}-precision build using eigen {}" , precision, OPERON_EIGEN_VERSION);// NOLINT
         if (OPERON_CERES_VERSION) {// NOLINT
-            fmt::format_to(buf, ", ceres {}", OPERON_CERES_VERSION);// NOLINT
+            fmt::format_to(std::back_inserter(buf), ", ceres {}", OPERON_CERES_VERSION);// NOLINT
         }// NOLINT
-        fmt::format_to(buf, ", taskflow {}", OPERON_TF_VERSION);// NOLINT
-        fmt::format_to(buf, "\n");
-        return std::string(buf.begin(), buf.end());
+        fmt::format_to(std::back_inserter(buf), ", taskflow {}", OPERON_TF_VERSION);// NOLINT
+        fmt::format_to(std::back_inserter(buf), "\n");
+        return { buf.begin(), buf.end() };
 }
 
 } // namespace Operon
