dc51025728263b2cba2016a69b6696c704fad3aa
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-08-24 00:49:05 +0200

    rank sort: refactor, optimize, use pdqsort with a stable adapter

--- source/operators/non_dominated_sorter/rank_intersect.cpp
+++ source/operators/non_dominated_sorter/rank_intersect.cpp
@@ -5,22 +5,25 @@
 #include "operon/operators/non_dominated_sorter.hpp"
 
 #include <iostream>
+#include <eve/wide.hpp>
+#include <cpp-sort/adapters/stable_adapter.h>
+#include <cpp-sort/sorters/pdq_sorter.h>
 
 namespace Operon {
 namespace detail {
-    template <typename Iter>
-    inline auto TightenBounds(Iter begin, Iter end) -> std::pair<Iter, Iter>
+    template<std::ranges::random_access_range R, typename T = typename std::remove_cvref_t<R>::value_type, size_t D = std::numeric_limits<T>::digits>
+    inline auto TightenBounds(R&& r, int lo, int hi, T v) -> std::pair<int, int>
     {
-        while (begin < end && *begin == 0) { ++begin; }
-        while (begin < end-1 && *(end-1) == 0) { --end; }
-        return { begin, end };
+        while (lo < hi && r[lo] == v) { ++lo; }
+        while (lo < hi-1 && r[hi-1] == v) { --hi; }
+        return { lo, hi };
     }
 
     template<std::ranges::random_access_range R, typename T = typename std::remove_cvref_t<R>::value_type, size_t D = std::numeric_limits<T>::digits>
     requires std::is_integral_v<T>
     inline auto SetBit(R&& r, int i)
     {
-        r[i / D] |= (T{1} << (i % D));
+        r[i / D] |= (T{1} << (D - i % D));
     }
 
     template<std::ranges::random_access_range R, typename T = typename std::remove_cvref_t<R>::value_type, size_t D = std::numeric_limits<T>::digits>
@@ -28,6 +31,27 @@ namespace detail {
     inline auto ResetBit(R&& r, int i)
     {
         r[i / D] &= ~(T{1} << (i % D));
+    }
+
+    template<std::ranges::random_access_range R, typename T = typename std::remove_cvref_t<R>::value_type, size_t D = std::numeric_limits<T>::digits>
+    inline auto Print(R&& r)
+    {
+        for (auto v : r) { std::cout << std::bitset<D>(v) << " "; }
+        std::cout << "\n";
+    }
+
+    template<typename T, typename X = std::remove_extent_t<T>, std::align_val_t ALIGNMENT = std::align_val_t{__STDCPP_DEFAULT_NEW_ALIGNMENT__}>
+    inline auto MakeUnique(size_t n)
+    {
+        return std::unique_ptr<T>(new (ALIGNMENT) X[n]);
+    }
+
+    template<typename T, typename X = std::remove_extent_t<T>, std::align_val_t ALIGNMENT = std::align_val_t{__STDCPP_DEFAULT_NEW_ALIGNMENT__}>
+    inline auto MakeUnique(size_t n, X value)
+    {
+        auto p = std::unique_ptr<T>(new (ALIGNMENT) X[n]);
+        std::fill_n(p.get(), n, value);
+        return p;
     }
 } // namespace detail
 
@@ -39,70 +63,87 @@ auto RankIntersectSorter::Sort(Operon::Span<Operon::Individual const> pop, Opero
 
     // bitset block type
     using Block = uint64_t;
-    auto constexpr ONE = ~Block{0};
-    auto constexpr ZERO = Block{0};
+    auto constexpr ONES = ~Block{0};
+    auto constexpr ZEROS = Block{0};
 
     // constants
     size_t constexpr DIGITS{std::numeric_limits<Block>::digits};
     size_t const nb{n / DIGITS + static_cast<size_t>(n % DIGITS != 0)};
+    size_t const ub = DIGITS * nb - n; // unused bit region (must be zeroed)
 
-    using Item = std::tuple<Operon::Scalar, int>;
     using Span = std::span<Block>;
-    using Iter = Span::iterator;
-    std::vector<std::pair<Iter, Iter>> bounds; bounds.reserve(n); // vector of ranges keeping track of the first/last non-zero blocks
+    std::vector<std::pair<int, int>> bounds(n); // vector of ranges keeping track of the first/last non-zero blocks
 
     // initialization
-    std::vector<Block> q(nb, ONE);
-    q[nb-1] >>= (DIGITS * nb - n); // zero unused region
+    auto aux = detail::MakeUnique<Block[]>(nb, ONES);
+    std::span<Block> q(aux.get(), nb);
+    q[nb-1] >>= ub; // zero unused region
 
-    std::unique_ptr<Block> bits(new Block[nb * n]); // allocate all memory at once (slightly more efficient)
-    std::vector<Item> items(n); // these items hold the values to be sorted along with their associated index
+    auto bits = detail::MakeUnique<Block[]>(nb * n);
+    std::vector<int> indices(n);
 
     auto b = 0; // current non-zero block
     for (auto i = 0; i < n; ++i) {
-        std::get<1>(items[i]) = i;
+        indices[i] = i;
         detail::ResetBit(q, i); // reset bit i
         b += static_cast<int>(q[i / DIGITS] == 0); // advance lower bound when a block becomes zero
         Span s{bits.get() + i * nb, nb};
-        std::copy_n(q.begin() + b, nb - b, s.begin() + b); // initialize bitset with q
-        bounds.emplace_back(s.begin() + b, s.begin() + nb); // NOLINT
+        std::copy(q.begin() + b, q.end(), s.begin() + b); // initialize bitset with q
+        bounds[i] = { b, nb }; // NOLINT
     }
 
-    std::vector<std::vector<Block>> ranksets{ std::vector(nb, ONE) }; // vector of sets keeping track of individuals whose rank was updated
-    ranksets.back()[nb-1] >>= DIGITS * nb - n; // zero unused region
+    std::vector<std::unique_ptr<Block[]>> ranksets;
+    ranksets.push_back(detail::MakeUnique<Block[]>(nb, ONES)); // vector of sets keeping track of individuals whose rank was updated
+    ranksets.back().get()[nb-1] >>= ub; // zero unused region
     std::vector<int> rank(n, 0); // individual ranks (initially, all zero)
-    auto cmp = [eps](auto a, auto b) { return Operon::Less{}(std::get<0>(a), std::get<0>(b), eps); };
+    Operon::Less cmp{};
 
-    std::vector<size_t> blocks; blocks.reserve(nb);
-    for (auto k = 1; k < m; ++k) {
-        for (auto& [v, i] : items) { v = pop[i][k]; } // update item values with current k-th objectives
-        std::stable_sort(items.begin(), items.end(), cmp); // sort items
-        std::fill_n(q.begin(), nb, ONE); // reset q bitset to all ones
-        q[nb-1] >>= DIGITS * nb - n; // zero unused region
+    std::vector<Operon::Scalar> values(n);
+    cppsort::stable_adapter<cppsort::pdq_sorter> sorter;
 
-        for (auto [_, i] : items) {
+    for (auto obj = 1; obj < m; ++obj) {
+        std::transform(pop.begin(), pop.end(), values.begin(), [obj](auto const& ind) { return ind[obj]; });
+        sorter(indices, [&](auto i) { return values[i]; });
+        std::fill(q.begin(), q.end(), ONES); // reset q bitset to all ones
+        q[nb-1] >>= ub; // zero unused region
+
+        for (auto i : indices) {
             detail::ResetBit(q, i); // reset bit i
+
+            if (i == indices.back()) {
+                // last individual cannot dominate anyone else
+                std::get<0>(bounds[i]) = std::get<1>(bounds[i]);
+                continue;
+            }
             auto [lo, hi] = bounds[i]; // get bounds
             if (lo == hi) { continue; }
-            Span s{bits.get() + i * nb, nb};
+
             // compute the intersections of the dominance sets (most runtime intensive part)
-            std::transform(lo, hi, q.begin() + std::distance(s.begin(), lo), lo, std::bit_and{});
-            bounds[i] = detail::TightenBounds(lo, hi); // update bounds
+            Span s{bits.get() + i * nb, nb};
+            if (i != indices.front()) {
+                auto* ss = s.data();
+                auto* qq = q.data();
 
-            if (k == m-1) { // if we're at the last objective, we can update the ranks
-                if (rank[i] + 1UL == ranksets.size()) { // new rankset if necessary
-                    ranksets.emplace_back(nb, ZERO); // NOLINT
+                for (auto j = lo; j < hi; ++j) {
+                    s[j] &= q[j];
                 }
-                auto curr = ranksets.begin() + rank[i]; // the pareto front of the current individual
-                auto next = curr + 1;                   // next (worse) pareto front
+                bounds[i] = detail::TightenBounds(s, lo, hi, ZEROS);              // update bounds
+            }
 
-                auto j = std::distance(s.begin(), bounds[i].first);
-                for (auto it = bounds[i].first; it != bounds[i].second; ++it, ++j) { // iterate over bitset blocks
-                    auto v = *it & (*curr)[j];                                       // final set as intersection of dominance set and rank set
-                    (*curr)[j] &= ~v;                                                // remove intersection result from current rank set
-                    (*next)[j] |= v;                                                 // add intersection result to next rank set
-                    for (; v != 0; v &= (v - 1)) {                                   // iterate over set bits of v
-                        ++rank[j * DIGITS + std::countr_zero(v)];                    // increment rank
+            if (obj == m-1) {
+                // if we're at the last objective, we can update the ranks
+                if (rank[i] + 1UL == ranksets.size()) {                         // new rankset if necessary
+                    ranksets.push_back(detail::MakeUnique<Block[]>(nb, ZEROS)); // NOLINT
+                }
+                auto* curr = (ranksets.begin() + rank[i])->get();               // the pareto front of the current individual
+                auto* next = (ranksets.begin() + rank[i] + 1)->get();           // next (worse) pareto front
+
+                for (auto j = lo; j != hi; ++j) {                               // iterate over bitset blocks
+                    auto v = s[j] & curr[j];                                    // final set as intersection of dominance set and rank set
+                    curr[j] &= ~v;                                              // remove intersection result from current rank set
+                    next[j] |= v;                                               // add intersection result to next rank set
+                    for (; v != 0; v &= (v - 1)) {                              // iterate over set bits of v
+                        ++rank[j * DIGITS + std::countr_zero(v)];               // increment rank
                     }
                 }
             }
