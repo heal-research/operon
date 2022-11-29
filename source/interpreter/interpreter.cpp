117616c5e545f0f72800c676892d90ba6ae4f60e
Bogdan Burlacu <bogdan.burlacu@pm.me>
2022-11-29 12:29:29 +0100

    interpreter: add EvaluateTrees method

--- source/interpreter/interpreter.cpp
+++ source/interpreter/interpreter.cpp
@@ -5,14 +5,33 @@
 #include "operon/interpreter/interpreter.hpp"
 
 namespace Operon {
-    auto Evaluate(Interpreter const& interpreter, std::vector<Operon::Tree> const& trees, Dataset const& dataset, Range range, Operon::Span<Operon::Scalar> result, size_t nthreads) noexcept -> void {
-        tf::Executor ex(nthreads);
-        tf::Taskflow tf;
-        auto const s = range.Size();
-        auto const n = trees.size();
-        tf.for_each_index(decltype(n){0}, n, decltype(n){1}, [&](size_t i) {
-            interpreter.Evaluate<Operon::Scalar>(trees[i], dataset, range, result.subspan(i * s, s));
+    auto EvaluateTrees(std::vector<Operon::Tree> const& trees, Operon::Dataset const& dataset, Operon::Range range, size_t nthread) -> std::vector<std::vector<Operon::Scalar>> {
+        if (nthread == 0) { nthread = std::thread::hardware_concurrency(); }
+        tf::Executor executor(nthread);
+        tf::Taskflow taskflow;
+        std::vector<std::vector<Operon::Scalar>> result(trees.size());
+        Operon::Interpreter interpreter;
+
+        taskflow.for_each_index(size_t{0}, size_t{trees.size()}, size_t{1}, [&](size_t i) {
+            result[i].resize(range.Size());
+            interpreter.Evaluate<Operon::Scalar>(trees[i], dataset, range, result[i]);
         });
-        ex.run(tf).wait();
+        executor.run(taskflow);
+        executor.wait_for_all();
+        return result;
+    }
+
+    auto EvaluateTrees(std::vector<Operon::Tree> const& trees, Operon::Dataset const& dataset, Operon::Range range, Operon::Span<Operon::Scalar> result, size_t nthread) -> void {
+        if (nthread == 0) { nthread = std::thread::hardware_concurrency(); }
+        tf::Executor executor(nthread);
+        tf::Taskflow taskflow;
+        Operon::Interpreter interpreter;
+
+        taskflow.for_each_index(size_t{0}, size_t{trees.size()}, size_t{1}, [&](size_t i) {
+            auto res = result.subspan(i * range.Size(), range.Size());
+            interpreter.Evaluate<Operon::Scalar>(trees[i], dataset, range, res);
+        });
+        executor.run(taskflow);
+        executor.wait_for_all();
     }
 } // namespace Operon
