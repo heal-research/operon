473e67dca6042f1f95acbe55d32c53a9c69cf009
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-03-31 19:33:49 +0200

    simplify metrics code

--- include/operon/error_metrics/normalized_mean_squared_error.hpp
+++ include/operon/error_metrics/normalized_mean_squared_error.hpp
@@ -20,13 +20,11 @@ inline auto NormalizedMeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 
     static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
     static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
     static_assert(std::is_same_v<V1, V2>, "The types must be the same");
-
-    constexpr double eps{1e-12};
     auto varY = vstat::univariate::accumulate<V1>(begin2, begin2 + std::distance(begin1, end1)).variance;
-    if (std::abs(varY) < eps) {
-        return varY;
+    if (varY > 0) {
+        return MeanSquaredError(begin1, end1, begin2) / varY;
     }
-    return MeanSquaredError(begin1, end1, begin2) / varY;
+    return 0.0;
 }
 
 template<typename T>
--- include/operon/error_metrics/r2_score.hpp
+++ include/operon/error_metrics/r2_score.hpp
@@ -18,15 +18,10 @@ inline auto R2Score(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept ->
     static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
     static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
     static_assert(std::is_same_v<V1, V2>, "The types must be the same");
-    constexpr double eps{1e-12};
-    auto ssr = vstat::univariate::accumulate<V1>(begin1, end1, begin2, [](auto a, auto b) { auto e = a - b; return e * e; }).sum;
-    auto end2 = begin2;
-    std::advance(end2, std::distance(begin1, end1));
-    auto meanY = vstat::univariate::accumulate<V2>(begin2, end2).mean;
-    auto sst = vstat::univariate::accumulate<V2>(begin2, end2, [&](auto v) { auto e = v - meanY; return e * e;} ).sum;
-    if (sst < eps) {
-        return std::numeric_limits<double>::lowest();
-    }
+    using vstat::univariate::accumulate;
+    auto const ssr = accumulate<V1>(begin1, end1, begin2, std::minus{}).ssr;
+    auto const sst = accumulate<V2>(begin2, begin2 + std::distance(begin1, end1)).ssr;
+    if (sst < std::numeric_limits<double>::epsilon()) { return std::numeric_limits<double>::lowest(); }
     return 1.0 - ssr / sst;
 }
 
