fb2cccfd5ddb6f5409d3e6bfb4bc7714c945a682
Bogdan Burlacu <bogdan.burlacu@pm.me>
2023-06-27 09:44:08 +0200

    implement weighted versions of error metrics

--- include/operon/error_metrics/mean_squared_error.hpp
+++ include/operon/error_metrics/mean_squared_error.hpp
@@ -7,6 +7,7 @@
 #include <iterator>
 #include <type_traits>
 #include <vstat/vstat.hpp>
+
 #include "operon/core/types.hpp"
 
 namespace Operon {
@@ -19,13 +20,20 @@ inline auto MeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) no
     static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
     static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
     static_assert(std::is_same_v<V1, V2>, "The types must be the same");
-    return vstat::univariate::accumulate<V1>(begin1, end1, begin2, std::minus{}).variance;
+    auto sqres = [](auto a, auto b){ auto e = a-b; return e*e; };
+    return vstat::univariate::accumulate<V1>(begin1, end1, begin2, sqres).mean;
 }
 
-template<typename InputIt1, typename InputIt2>
-inline auto RootMeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
+template<typename InputIt1, typename InputIt2, typename InputIt3>
+inline auto MeanSquaredError(InputIt1 begin1, InputIt1 end1, InputIt2 begin2, InputIt3 begin3) noexcept -> double
 {
-    return std::sqrt(MeanSquaredError(begin1, end1, begin2));
+    using V1 = typename std::iterator_traits<InputIt1>::value_type;
+    using V2 = typename std::iterator_traits<InputIt2>::value_type;
+    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
+    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
+    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
+    auto sqres = [](auto a, auto b){ auto e = a-b; return e*e; };
+    return vstat::univariate::accumulate<V1>(begin1, end1, begin2, begin3, sqres).mean;
 }
 
 template<typename T>
@@ -34,13 +42,16 @@ inline auto MeanSquaredError(Operon::Span<T const> x, Operon::Span<T const> y) n
     static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
     EXPECT(x.size() == y.size());
     EXPECT(!x.empty());
-    return vstat::univariate::accumulate<T>(x.data(), y.data(), x.size(), std::minus{}).variance;
+    return MeanSquaredError(x.begin(), x.end(), y.begin());
 }
 
 template<typename T>
-inline auto RootMeanSquaredError(Operon::Span<T const> x, Operon::Span<T const> y) noexcept -> double
+inline auto MeanSquaredError(Operon::Span<T const> x, Operon::Span<T const> y, Operon::Span<T const> w) noexcept -> double
 {
-    return std::sqrt(MeanSquaredError(x, y));
+    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
+    EXPECT(x.size() == y.size());
+    EXPECT(!x.empty());
+    return MeanSquaredError(x.begin(), x.end(), y.begin(), w.begin());
 }
 
 } // namespace Operon
