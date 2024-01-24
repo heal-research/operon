8080aa2115940a0e4062605dc33907d366ec1133
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-01-24 20:21:07 +0100

    cli programs: allow selection of the objective in the command line

--- cli/source/operator_factory.hpp
+++ cli/source/operator_factory.hpp
@@ -37,7 +37,7 @@ auto ParseSelector(std::string const& str, ComparisonCallback&& comp) -> std::un
 
 auto ParseCreator(std::string const& str, PrimitiveSet const& pset, std::vector<Operon::Hash> const& inputs) -> std::unique_ptr<CreatorBase>;
 
-auto ParseEvaluator(std::string const& str, Problem& problem, DefaultDispatch& dtable) -> std::unique_ptr<EvaluatorBase>;
+auto ParseEvaluator(std::string const& str, Problem& problem, DefaultDispatch& dtable, bool scale = true) -> std::unique_ptr<EvaluatorBase>;
 
 auto ParseErrorMetric(std::string const& str) -> std::tuple<std::unique_ptr<Operon::ErrorMetric>, bool>;
 
