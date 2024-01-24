8e6314ea9553bdfd5ac8560d8ab55679c854e799
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-01-24 19:31:57 +0100

    simplify usage of error metrics in the evlauator

---
+++ source/operators/evaluator_error_metrics.cpp
@@ -0,0 +1,52 @@
+#include "operon/operators/evaluator.hpp"
+#include "operon/error_metrics/error_metrics.hpp"
+
+namespace Operon {
+    auto ErrorMetric::operator()(Operon::Span<Operon::Scalar const> x, Operon::Span<Operon::Scalar const> y) const noexcept -> double {
+        switch (type_) {
+        case ErrorType::SSE: return SumOfSquaredErrors(x, y);
+        case ErrorType::MSE: return MeanSquaredError(x, y);
+        case ErrorType::NMSE: return NormalizedMeanSquaredError(x, y);
+        case ErrorType::RMSE: return RootMeanSquaredError(x, y);
+        case ErrorType::MAE: return MeanAbsoluteError(x, y);
+        case ErrorType::R2: return -R2Score(x, y);
+        case ErrorType::C2: return -SquaredCorrelation(x, y);
+        }
+    }
+
+    auto ErrorMetric::operator()(Operon::Span<Operon::Scalar const> x, Operon::Span<Operon::Scalar const> y, Operon::Span<Operon::Scalar const> w) const noexcept -> double {
+        switch (type_) {
+        case ErrorType::SSE: return SumOfSquaredErrors(x, y, w);
+        case ErrorType::MSE: return MeanSquaredError(x, y, w);
+        case ErrorType::NMSE: return NormalizedMeanSquaredError(x, y, w);
+        case ErrorType::RMSE: return RootMeanSquaredError(x, y, w);
+        case ErrorType::MAE: return MeanAbsoluteError(x, y, w);
+        case ErrorType::R2: return -R2Score(x, y, w);
+        case ErrorType::C2: return -SquaredCorrelation(x, y, w);
+        }
+    }
+
+    auto ErrorMetric::operator()(Iterator beg1, Iterator end1, Iterator beg2) const noexcept -> double {
+        switch (type_) {
+        case ErrorType::SSE: return SumOfSquaredErrors(beg1, end1, beg2);
+        case ErrorType::MSE: return MeanSquaredError(beg1, end1, beg2);
+        case ErrorType::NMSE: return NormalizedMeanSquaredError(beg1, end1, beg2);
+        case ErrorType::RMSE: return RootMeanSquaredError(beg1, end1, beg2);
+        case ErrorType::MAE: return MeanAbsoluteError(beg1, end1, beg2);
+        case ErrorType::R2: return -R2Score(beg1, end1, beg2);
+        case ErrorType::C2: return -SquaredCorrelation(beg1, end1, beg2);
+        }
+    }
+
+    auto ErrorMetric::operator()(Iterator beg1, Iterator end1, Iterator beg2, Iterator beg3) const noexcept -> double {
+        switch (type_) {
+        case ErrorType::SSE: return SumOfSquaredErrors(beg1, end1, beg2, beg3);
+        case ErrorType::MSE: return MeanSquaredError(beg1, end1, beg2, beg3);
+        case ErrorType::NMSE: return NormalizedMeanSquaredError(beg1, end1, beg2, beg3);
+        case ErrorType::RMSE: return RootMeanSquaredError(beg1, end1, beg2, beg3);
+        case ErrorType::MAE: return MeanAbsoluteError(beg1, end1, beg2, beg3);
+        case ErrorType::R2: return -R2Score(beg1, end1, beg2, beg3);
+        case ErrorType::C2: return -SquaredCorrelation(beg1, end1, beg2, beg3);
+        }
+    }
+}  // namespace Operon
