44b4119334f723f69e7fd4eb3e81d2d594215b7a
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-05-16 14:50:56 +0200

    add math backends: arma, blaze

---
+++ include/operon/interpreter/backend/arma.hpp
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright 2019-2024 Heal Research
+
+#ifndef OPERON_BACKEND_ARMA_HPP
+#define OPERON_BACKEND_ARMA_HPP
+
+#include "arma/derivatives.hpp"
+
+#endif
