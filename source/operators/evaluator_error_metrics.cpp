7b6e21987c99c057e17ff9771019f83d8837f9e1
Bogdan Burlacu <bogdan.burlacu@pm.me>
2024-05-27 12:54:57 +0200

    fix some warnings

--- source/operators/evaluator_error_metrics.cpp
+++ source/operators/evaluator_error_metrics.cpp
@@ -2,7 +2,7 @@
 #include "operon/error_metrics/error_metrics.hpp"
 
 namespace Operon {
-    auto ErrorMetric::operator()(Operon::Span<Operon::Scalar const> x, Operon::Span<Operon::Scalar const> y) const noexcept -> double {
+    auto ErrorMetric::operator()(Operon::Span<Operon::Scalar const> x, Operon::Span<Operon::Scalar const> y) const -> double {
         switch (type_) {
         case ErrorType::SSE: return SumOfSquaredErrors(x, y);
         case ErrorType::MSE: return MeanSquaredError(x, y);
@@ -11,10 +11,11 @@ namespace Operon {
         case ErrorType::MAE: return MeanAbsoluteError(x, y);
         case ErrorType::R2: return -R2Score(x, y);
         case ErrorType::C2: return -SquaredCorrelation(x, y);
+        default: { throw std::runtime_error("unknown error type"); }
         }
     }
 
-    auto ErrorMetric::operator()(Operon::Span<Operon::Scalar const> x, Operon::Span<Operon::Scalar const> y, Operon::Span<Operon::Scalar const> w) const noexcept -> double {
+    auto ErrorMetric::operator()(Operon::Span<Operon::Scalar const> x, Operon::Span<Operon::Scalar const> y, Operon::Span<Operon::Scalar const> w) const -> double {
         switch (type_) {
         case ErrorType::SSE: return SumOfSquaredErrors(x, y, w);
         case ErrorType::MSE: return MeanSquaredError(x, y, w);
@@ -23,10 +24,11 @@ namespace Operon {
         case ErrorType::MAE: return MeanAbsoluteError(x, y, w);
         case ErrorType::R2: return -R2Score(x, y, w);
         case ErrorType::C2: return -SquaredCorrelation(x, y, w);
+        default: { throw std::runtime_error("unknown error type"); }
         }
     }
 
-    auto ErrorMetric::operator()(Iterator beg1, Iterator end1, Iterator beg2) const noexcept -> double {
+    auto ErrorMetric::operator()(Iterator beg1, Iterator end1, Iterator beg2) const -> double {
         switch (type_) {
         case ErrorType::SSE: return SumOfSquaredErrors(beg1, end1, beg2);
         case ErrorType::MSE: return MeanSquaredError(beg1, end1, beg2);
@@ -35,10 +37,11 @@ namespace Operon {
         case ErrorType::MAE: return MeanAbsoluteError(beg1, end1, beg2);
         case ErrorType::R2: return -R2Score(beg1, end1, beg2);
         case ErrorType::C2: return -SquaredCorrelation(beg1, end1, beg2);
+        default: { throw std::runtime_error("unknown error type"); }
         }
     }
 
-    auto ErrorMetric::operator()(Iterator beg1, Iterator end1, Iterator beg2, Iterator beg3) const noexcept -> double {
+    auto ErrorMetric::operator()(Iterator beg1, Iterator end1, Iterator beg2, Iterator beg3) const -> double {
         switch (type_) {
         case ErrorType::SSE: return SumOfSquaredErrors(beg1, end1, beg2, beg3);
         case ErrorType::MSE: return MeanSquaredError(beg1, end1, beg2, beg3);
@@ -47,6 +50,7 @@ namespace Operon {
         case ErrorType::MAE: return MeanAbsoluteError(beg1, end1, beg2, beg3);
         case ErrorType::R2: return -R2Score(beg1, end1, beg2, beg3);
         case ErrorType::C2: return -SquaredCorrelation(beg1, end1, beg2, beg3);
+        default: { throw std::runtime_error("unknown error type"); }
         }
     }
 }  // namespace Operon
