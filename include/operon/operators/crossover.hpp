cdf1f978fb51631148b2d106acac0159e00cb490
Bogdan Burlacu <bogdan.burlacu@pm.me>
2025-02-16 20:35:17 +0100

    move static crossover helper methods in base class

--- include/operon/operators/crossover.hpp
+++ include/operon/operators/crossover.hpp
@@ -20,33 +20,21 @@
 namespace Operon {
 // crossover takes two parent trees and returns a child
 struct CrossoverBase : public OperatorBase<Tree, const Tree&, const Tree&> {
+    using Limits = std::pair<std::size_t, std::size_t>;
+    static auto FindCompatibleSwapLocations(Operon::RandomGenerator& random, Tree const& lhs, Tree const& rhs, size_t maxDepth, size_t maxLength, double internalProbability = 1.0) -> std::pair<size_t, size_t>;
+    static auto SelectRandomBranch(Operon::RandomGenerator& random, Tree const& tree, double internalProb, Limits length, Limits level, Limits depth) -> size_t;
+    static auto Cross(const Tree& lhs, const Tree& rhs, /* index of subtree 1 */ size_t i, /* index of subtree 2 */ size_t j) -> Tree;
 };
 
 class OPERON_EXPORT SubtreeCrossover : public CrossoverBase {
 public:
-    SubtreeCrossover(double p, size_t d, size_t l)
-        : internalProbability_(p)
-        , maxDepth_(d)
-        , maxLength_(l)
-    {
-    }
+    SubtreeCrossover(double internalProbability, size_t maxDepth, size_t maxLength)
+        : internalProbability_(internalProbability)
+        , maxDepth_(maxDepth)
+        , maxLength_(maxLength)
+    { }
+
     auto operator()(Operon::RandomGenerator& random, const Tree& lhs, const Tree& rhs) const -> Tree override;
-    auto FindCompatibleSwapLocations(Operon::RandomGenerator& random, const Tree& lhs, const Tree& rhs) const -> std::pair<size_t, size_t>;
-
-    static inline auto Cross(const Tree& lhs, const Tree& rhs, /* index of subtree 1 */ size_t i, /* index of subtree 2 */ size_t j) -> Tree
-    {
-        auto const& left = lhs.Nodes();
-        auto const& right = rhs.Nodes();
-        Operon::Vector<Node> nodes;
-        using signed_t = std::make_signed<size_t>::type; // NOLINT
-        nodes.reserve(right[j].Length - left[i].Length + left.size());
-        std::copy_n(left.begin(), i - left[i].Length, back_inserter(nodes));
-        std::copy_n(right.begin() + static_cast<signed_t>(j) - right[j].Length, right[j].Length + 1, back_inserter(nodes));
-        std::copy_n(left.begin() + static_cast<signed_t>(i) + 1, left.size() - (i + 1), back_inserter(nodes));
-
-        auto child = Tree(nodes).UpdateNodes();
-        return child;
-    }
 
     [[nodiscard]] auto InternalProbability() const -> double { return internalProbability_; }
     [[nodiscard]] auto MaxDepth() const -> size_t { return maxDepth_; }
@@ -57,5 +45,6 @@ private:
     size_t maxDepth_;
     size_t maxLength_;
 };
+
 } // namespace Operon
 #endif
