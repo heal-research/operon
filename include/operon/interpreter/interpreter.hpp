f716b94b90aa3ca752db67b57f212921493ce127
Bogdan Burlacu <bogdan.burlacu@pm.me>
2025-04-02 17:09:28 +0200

    code cleanup in interpreter.hpp

--- include/operon/interpreter/interpreter.hpp
+++ include/operon/interpreter/interpreter.hpp
@@ -114,25 +114,24 @@ struct Interpreter : public InterpreterBase<T> {
 
     auto JacFwd(Operon::Span<T const> coeff, Operon::Range range, Operon::Span<T> jacobian) const -> void final {
         InitContext(coeff, range);
-        auto const len{ static_cast<int>(range.Size()) };
         auto const& nodes = tree_->Nodes();
-        auto const nn   { std::ssize(nodes) };
+        auto const nNodes = std::ssize(nodes); 
+        auto const nRows  = static_cast<int>(range.Size());
 
-        constexpr int64_t S{ BatchSize };
-        trace_ = Backend::Buffer<T, S>(S, nn);
-        Backend::Fill<T, S>(trace_, nn-1, T{1});
+        trace_ = Backend::Buffer<T, BatchSize>(BatchSize, nNodes);
+        Backend::Fill<T, BatchSize>(trace_, nNodes-1, T{1});
 
-        Eigen::Map<Eigen::Array<T, -1, -1>> jac(jacobian.data(), len, coeff.size());
+        Eigen::Map<Eigen::Array<T, -1, -1>> jac(jacobian.data(), nRows, coeff.size());
 
-        for (int row = 0; row < len; row += S) {
+        for (int row = 0; row < nRows; row += BatchSize) {
             ForwardPass(range, row, /*trace=*/true);
             ForwardTrace(range, row, jac);
         }
     }
 
     auto JacFwd(Operon::Span<T const> coeff, Operon::Range range) const -> Eigen::Array<T, -1, -1> final {
-        auto const nr{ static_cast<int64_t>(range.Size()) };
-        Eigen::Array<T, -1, -1> jacobian(nr, coeff.size());
+        auto const nRows = static_cast<int64_t>(range.Size());
+        Eigen::Array<T, -1, -1> jacobian(nRows, coeff.size());
         JacFwd(coeff, range, { jacobian.data(), static_cast<size_t>(jacobian.size()) });
         return jacobian;
     }
@@ -166,23 +165,22 @@ private:
 
     // mutable internal state (used by all the forward/reverse passes)
     mutable Operon::Vector<Data> context_;
-
     mutable Backend::Buffer<T, BatchSize> primal_;
     mutable Backend::Buffer<T, BatchSize> trace_;
 
     // private methods
     auto ForwardPass(Operon::Range range, int row, bool trace = false) const -> void {
-        auto const start { static_cast<int64_t>(range.Start()) };
-        auto const len   { static_cast<int64_t>(range.Size()) };
-        auto const& nodes = tree_->Nodes();
-        auto const nn = std::ssize(nodes);
-        constexpr int64_t S{ BatchSize };
+        auto const& nodes     = tree_->Nodes();
+        auto const nNodes     = std::ssize(nodes);
+        auto const rangeStart = static_cast<int64_t>(range.Start());
+        auto const rangeSize  = static_cast<int64_t>(range.Size());
+        constexpr int64_t S   = BatchSize;
 
-        auto rem = std::min(S, len - row);
-        Operon::Range rg(start + row, start + row + rem);
+        auto rem = std::min(S, rangeSize - row);
+        Operon::Range rg(rangeStart + row, rangeStart + row + rem);
 
         // forward pass - compute primal and trace
-        for (auto i = 0L; i < nn; ++i) {
+        for (auto i = 0L; i < nNodes; ++i) {
             if (nodes[i].IsConstant()) { continue; }
 
             auto const& [ p, v, f, df ] = context_[i];
@@ -209,86 +207,86 @@ private:
     }
 
     auto ForwardTrace(Operon::Range range, int row, Eigen::Ref<Eigen::Array<T, -1, -1>> jac) const -> void {
-        auto const len   { static_cast<int64_t>(range.Size()) };
-        auto const& nodes{ tree_->Nodes() };
-        auto const nn { std::ssize(nodes) };
-        constexpr int64_t S{ BatchSize };
-        auto const rem   { std::min(S, len - row) };
+        auto const rangeSize     = static_cast<int64_t>(range.Size());
+        auto const& nodes        = tree_->Nodes();
+        auto const nNodes        = std::ssize(nodes);
+        constexpr int64_t S      = BatchSize;
+        auto const remainingRows = std::min(S, rangeSize - row);
 
-        Eigen::Array<T, S, -1> dot(S, nn);
+        Eigen::Array<T, S, -1> dot(S, nNodes);
         Operon::Vector<int64_t> cidx(jac.cols());
 
-        Eigen::Map<Eigen::Array<T, S, -1>> primal(primal_.data(), S, nn);
-        Eigen::Map<Eigen::Array<T, S, -1>> trace(trace_.data(), S, nn);
+        Eigen::Map<Eigen::Array<T, S, -1>> primal(primal_.data(), S, nNodes);
+        Eigen::Map<Eigen::Array<T, S, -1>> trace(trace_.data(), S, nNodes);
 
-        for (auto i = 0L, j = 0L; i < nn; ++i) {
+        for (auto i = 0L, j = 0L; i < nNodes; ++i) {
             if (nodes[i].Optimize) { cidx[j++] = i; }
         }
 
         auto k{0};
         for (auto c : cidx) {
-            dot.topRows(rem).setConstant(T{0});
-            dot.col(c).head(rem).setConstant(T{1});
+            dot.topRows(remainingRows).setConstant(T{0});
+            dot.col(c).head(remainingRows).setConstant(T{1});
 
-            for (auto i = 0; i < nn; ++i) {
+            for (auto i = 0; i < nNodes; ++i) {
                 if (nodes[i].IsLeaf()) { continue; }
                 for (auto x : Tree::Indices(nodes, i)) {
                     auto j{ static_cast<int64_t>(x) };
                     if (nodes[j].IsLeaf() && j != c) { continue; }
-                    dot.col(i).head(rem) += dot.col(j).head(rem) * trace.col(j).head(rem) * std::get<0>(context_[i]);
+                    dot.col(i).head(remainingRows) += dot.col(j).head(remainingRows) * trace.col(j).head(remainingRows) * std::get<0>(context_[i]);
                 }
             }
 
-            jac.col(k++).segment(row, rem) = dot.col(nn-1).head(rem) * primal.col(c).head(rem) / std::get<0>(context_[c]);
+            jac.col(k++).segment(row, remainingRows) = dot.col(nNodes-1).head(remainingRows) * primal.col(c).head(remainingRows) / std::get<0>(context_[c]);
         }
     }
 
     auto ReverseTrace(Operon::Range range, int row, Eigen::Ref<Eigen::Array<T, -1, -1>> jac) const -> void {
-        auto const len   { static_cast<int64_t>(range.Size()) };
-        auto const& nodes{ tree_->Nodes() };
-        auto const nn    { std::ssize(nodes) };
-        constexpr int64_t S{ BatchSize };
-        auto const rem   { std::min(S, len - row) };
+        auto const rangeSize     = static_cast<int64_t>(range.Size());
+        auto const& nodes        = tree_->Nodes();
+        auto const nNodes        = std::ssize(nodes);
+        constexpr int64_t S      = BatchSize;
+        auto const remainingRows = std::min(S, rangeSize - row);
 
         auto k{jac.cols()};
-        Eigen::Map<Eigen::Array<T, S, -1>> primal(primal_.data(), S, nn);
-        Eigen::Map<Eigen::Array<T, S, -1>> trace(trace_.data(), S, nn);
+        Eigen::Map<Eigen::Array<T, S, -1>> primal(primal_.data(), S, nNodes);
+        Eigen::Map<Eigen::Array<T, S, -1>> trace(trace_.data(), S, nNodes);
 
-        for (auto i = nn-1; i >= 0L; --i) {
+        for (auto i = nNodes-1; i >= 0L; --i) {
             auto w = std::get<0>(context_[i]);
 
             if (nodes[i].Optimize) {
-                jac.col(--k).segment(row, rem) = trace.col(i).head(rem) * primal.col(i).head(rem) / w;
+                jac.col(--k).segment(row, remainingRows) = trace.col(i).head(remainingRows) * primal.col(i).head(remainingRows) / w;
             }
 
             if (nodes[i].IsLeaf()) { continue; }
 
             for (auto j : Tree::Indices(nodes, i)) {
                 auto const x { static_cast<int64_t>(j) };
-                trace.col(x).head(rem) *= trace.col(i).head(rem) * w;
+                trace.col(x).head(remainingRows) *= trace.col(i).head(remainingRows) * w;
             }
         }
     }
 
     // init tree info into context_ and initializes primal_ columns
     auto InitContext(Operon::Span<T const> coeff, Operon::Range range) const {
-        auto const& nodes{ tree_->Nodes() };
-        auto const nr { static_cast<int64_t>(range.Size()) };
-        auto const nn { std::ssize(nodes) };
+        auto const& nodes = tree_->Nodes();
+        auto const nRows  = static_cast<int64_t>(range.Size());
+        auto const nNodes = std::ssize(nodes);
 
         constexpr int64_t S{ BatchSize };
-        primal_ = Backend::Buffer<T, S>(S, nn);
-        std::ranges::fill_n(primal_.data(), S * nn, T{0});
+        primal_ = Backend::Buffer<T, S>(S, nNodes);
+        std::ranges::fill_n(primal_.data(), S * nNodes, T{0});
 
         context_.clear();
-        context_.reserve(nn);
+        context_.reserve(nNodes);
 
         auto const& dt = dtable_.get();
         // aggregate necessary info about the tree into a context object
-        for (int64_t i = 0, j = 0; i < nn; ++i) {
+        for (int64_t i = 0, j = 0; i < nNodes; ++i) {
             auto const& n = nodes[i];
             auto const* ptr      = n.IsVariable() ? dataset_->GetValues(n.HashValue).subspan(range.Start(), range.Size()).data() : nullptr;
-            auto variableValues  = std::tuple_element_t<1, Data>(ptr, nr);
+            auto variableValues  = std::tuple_element_t<1, Data>(ptr, nRows);
             auto nodeCoefficient = (!coeff.empty() && n.Optimize) ? T{coeff[j++]} : T{n.Value};
             auto nodeFunction    = dt->template TryGetFunction<T>(n.HashValue);
             auto nodeDerivative  = dt->template TryGetDerivative<T>(n.HashValue);
