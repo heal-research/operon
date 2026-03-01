f093193564a2c96009249a2eff585bfbe98c085c
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-03-01 12:37:29 +0200

    update vcpkg ports and config

--- ports/fluky/portfile.cmake
+++ ports/fluky/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO foolnotion/pratt-parser-calculator 
-    REF 2e0b13615c6ff1fb6381c0ac87796932b326bc89
-    SHA512 bdb910209c1936897769f00ca12c3f054068360bb811238e117f862519ea41632688c66a8c78e7763f145537a489e9df00d37728ec75379a2e4b1078106750be
+    REPO foolnotion/fluky
+    REF 19320e7499cf0958268dc11fec28a6e41ac332e4
+    SHA512 466f40f5aaa9b4bce66a6daeb6141c17f68972b529ac71db96545d87b50701c8a2fa7112b2f4250b98e7bf516e738a5562cfa1a18dc69a803ca21a8494755ae5
     HEAD_REF main
 )
 
@@ -20,7 +20,7 @@ vcpkg_configure_cmake(
         -DBUILD_EXAMPLES=OFF
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME pratt-parser CONFIG_PATH lib/cmake/pratt-parser DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(PACKAGE_NAME fluky CONFIG_PATH lib/cmake/fluky DO_NOT_DELETE_PARENT_CONFIG_PATH)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
                     "${CURRENT_PACKAGES_DIR}/debug/share"
