dfc2fdd6d1c593dee7e49ee606ccfda2699a7b0c
Bogdan Burlacu <bogdan.burlacu@pm.me>
2026-06-03 10:51:33 +0300

    feat(concepts): apply concept constraints to error metrics and evaluators
    Replace static_assert+type_traits guards in all error metric free functions
    with requires clauses using Concepts::Arithmetic and std::same_as. Use
    Concepts::Arithmetic as a named template parameter on span overloads.
    
    Constrain the Lik template parameter of MinimumDescriptionLengthEvaluator
    and FractionalBayesFactorEvaluator with Concepts::Likelihood.

--- include/operon/error_metrics/correlation_coefficient.hpp
+++ include/operon/error_metrics/correlation_coefficient.hpp
@@ -5,71 +5,72 @@
 #define OPERON_METRICS_CORRELATION_COEFFICIENT_HPP
 
 #include <iterator>
-#include <type_traits>
 #include <vstat/vstat.hpp>
-#include "operon/core/types.hpp"
+#include "operon/core/concepts.hpp"
 
 namespace Operon {
 
 template<typename InputIt1, typename InputIt2>
+    requires Concepts::Arithmetic<typename std::iterator_traits<InputIt1>::value_type>
+          && std::same_as<typename std::iterator_traits<InputIt1>::value_type,
+                          typename std::iterator_traits<InputIt2>::value_type>
 inline auto CorrelationCoefficient(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double
 {
     using V1 = typename std::iterator_traits<InputIt1>::value_type;
-    using V2 = typename std::iterator_traits<InputIt2>::value_type;
-    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
-    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
-    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
     return vstat::bivariate::accumulate<V1>(begin1, end1, begin2).correlation;
 }
 
 template<typename InputIt1, typename InputIt2, typename InputIt3>
+    requires Concepts::Arithmetic<typename std::iterator_traits<InputIt1>::value_type>
+          && std::same_as<typename std::iterator_traits<InputIt1>::value_type,
+                          typename std::iterator_traits<InputIt2>::value_type>
 inline auto CorrelationCoefficient(InputIt1 begin1, InputIt1 end1, InputIt2 begin2, InputIt3 begin3) noexcept -> double
 {
     using V1 = typename std::iterator_traits<InputIt1>::value_type;
-    using V2 = typename std::iterator_traits<InputIt2>::value_type;
-    static_assert(std::is_arithmetic_v<V1>, "InputIt1: value_type must be arithmetic.");
-    static_assert(std::is_arithmetic_v<V2>, "InputIt2: value_type must be arithmetic.");
-    static_assert(std::is_same_v<V1, V2>, "The types must be the same");
     return vstat::bivariate::accumulate<V1>(begin1, end1, begin2, begin3).correlation;
 }
 
-template<typename T>
+template<Concepts::Arithmetic T>
 inline auto CorrelationCoefficient(Operon::Span<T const> x, Operon::Span<T const> y) -> double
 {
-    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
     EXPECT(x.size() == y.size());
     EXPECT(!x.empty());
-    return vstat::bivariate::accumulate<T>(x.begin(), x.end(), y.begin()).correlation;
+    return vstat::bivariate::accumulate<T>(x.data(), x.data() + x.size(), y.data()).correlation;
 }
 
-template<typename T>
+template<Concepts::Arithmetic T>
 inline auto CorrelationCoefficient(Operon::Span<T const> x, Operon::Span<T const> y, Operon::Span<T const> w) -> double
 {
-    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type.");
     EXPECT(x.size() == y.size());
     EXPECT(!x.empty());
-    return vstat::bivariate::accumulate<T>(x.begin(), x.end(), y.begin(), w.begin()).correlation;
+    return vstat::bivariate::accumulate<T>(x.data(), x.data() + x.size(), y.data(), w.data()).correlation;
 }
 
 template<typename InputIt1, typename InputIt2>
+    requires Concepts::Arithmetic<typename std::iterator_traits<InputIt1>::value_type>
+          && std::same_as<typename std::iterator_traits<InputIt1>::value_type,
+                          typename std::iterator_traits<InputIt2>::value_type>
 inline auto SquaredCorrelation(InputIt1 begin1, InputIt1 end1, InputIt2 begin2) noexcept -> double {
     auto r = CorrelationCoefficient(begin1, end1, begin2);
     return r * r;
 }
 
 template<typename InputIt1, typename InputIt2, typename InputIt3>
+    requires Concepts::Arithmetic<typename std::iterator_traits<InputIt1>::value_type>
+          && std::same_as<typename std::iterator_traits<InputIt1>::value_type,
+                          typename std::iterator_traits<InputIt2>::value_type>
 inline auto SquaredCorrelation(InputIt1 begin1, InputIt1 end1, InputIt2 begin2, InputIt3 begin3) noexcept -> double {
     auto r = CorrelationCoefficient(begin1, end1, begin2, begin3);
     return r * r;
 }
 
-template<typename T>
+template<Concepts::Arithmetic T>
 inline auto SquaredCorrelation(Operon::Span<T const> x, Operon::Span<T const> y) -> double {
     auto r = CorrelationCoefficient(x, y);
     return r * r;
 }
 
-template<typename T>
+template<Concepts::Arithmetic T>
 inline auto SquaredCorrelation(Operon::Span<T const> x, Operon::Span<T const> y, Operon::Span<T const> w) -> double {
     auto r = CorrelationCoefficient(x, y, w);
     return r * r;
